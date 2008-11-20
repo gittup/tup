4b4e3c8be7ad1a38695a5385fa71c7c94897d965
marf <marfey@gmail.com>
2008-11-20 18:54:28 -0500

    Added a multi-output test. This simulates a command that changes its output files. Though, I currently don't know how it should be handled, so it's skipped

---
+++ test/t6003-change-multi-output.sh
@@ -0,0 +1,38 @@
+#! /bin/sh -e
+
+echo "[33mSkip t6003 - not sure how this should work"
+exit 0
+
+. ../tup.sh
+cat > Makefile << HERE
+# Change the directory "." to nothing - all other actual directory names get
+# a / appended
+d := \$(if \$(filter .,\$(TUPWD)),,\$(TUPWD)/)
+
+all:
+	@echo "Run a command to generate multiple files"
+	tup link "tup wrap sh ok.sh"
+
+.PHONY: all
+HERE
+
+cat > ok.sh << HERE
+touch a
+touch b
+HERE
+
+tup touch ok.sh Makefile
+update
+check_exist a b
+check_not_exist c
+
+cat > ok.sh << HERE
+touch a
+touch c
+HERE
+
+touch touch ok.sh
+update
+
+check_exist a c
+check_not_exist b
