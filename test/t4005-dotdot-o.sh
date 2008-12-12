0a84e55998274d4ff87cb6158c63ad4a25f7e558
marf <marfey@gmail.com>
2008-12-11 21:33:34 -0500

    Added t4005 to test that a program can include a file in a parent subdirectory. This currently fails because the built-in.o node isn't created until after the directory is parsed. At some point I'll have to figure out how to parse those in order, or at least re-parse the ones that need to be changed.

---
+++ test/t4005-dotdot-o.sh
@@ -0,0 +1,23 @@
+#! /bin/sh -e
+
+. ../tup.sh
+
+cat > Tupfile << HERE
+: foreach *.c >> gcc -c \$f -o \$F.o >> \$F.o
+: *.o >> ld -r \$f -o built-in.o >> built-in.o
+HERE
+
+mkdir fs
+cat > fs/Tupfile << HERE
+: foreach *.c >> gcc -c \$f -o \$F.o >> \$F.o
+: *.o ../built-in.o >> gcc \$f -o prog >> prog
+HERE
+
+echo "int main(void) {return 0;}" > fs/main.c
+echo "void ext3fs(void) {}" > ext3.c
+echo "void ext4fs(void) {}" > ext4.c
+
+tup touch Tupfile ext3.c ext4.c fs/main.c fs/Tupfile
+update
+
+sym_check fs/prog main ext3fs ext4fs
