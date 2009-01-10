6b78cdca33428fd2753c438b9f0e6370015ec2ef
marf <marfey@gmail.com>
2009-01-10 18:18:12 -0500

    Added t6008 to test for a specific circular dependency issue in a Tupfile that was pretty easy to avoid

--- src/tup/parser.c
+++ src/tup/parser.c
@@ -56,6 +56,7 @@ static int execute_rules(struct list_head *rules, tupid_t dt);
 static int file_match(void *rule, struct db_node *dbn);
 static char *set_path(const char *name, const char *dir, int dirlen);
 static int do_rule(struct rule *r, struct name_list *nl, tupid_t dt);
+static void delete_name_list_entry(struct name_list_entry *nle);
 static char *tup_printf(const char *cmd, struct name_list *nl, const char *o);
 static char *eval(struct vardb *v, const char *string);
 
@@ -425,9 +426,7 @@ static int execute_rules(struct list_head *rules, tupid_t dt)
 			while(!list_empty(&r->namelist.entries)) {
 				nle = list_entry(r->namelist.entries.next,
 						struct name_list_entry, list);
-				list_del(&nle->list);
-				free(nle->path);
-				free(nle);
+				delete_name_list_entry(nle);
 			}
 		}
 	}
@@ -522,7 +521,7 @@ static char *set_path(const char *name, const char *dir, int dirlen)
 
 static int do_rule(struct rule *r, struct name_list *nl, tupid_t dt)
 {
-	struct name_list_entry *nle;
+	struct name_list_entry *nle, *tmp;
 	char *cmd;
 	char *output;
 	int node_created = 0;
@@ -533,21 +532,6 @@ static int do_rule(struct rule *r, struct name_list *nl, tupid_t dt)
 	if(!output)
 		return -1;
 
-	cmd = tup_printf(r->command, nl, output);
-	if(!cmd)
-		return -1;
-	cmd_id = create_command_file(dt, cmd);
-	free(cmd);
-	if(cmd_id < 0)
-		return -1;
-
-	list_for_each_entry(nle, &nl->entries, list) {
-		printf("Match[%lli]: %s, %s\n", nl->dt, r->input_pattern,
-		       nle->path);
-		if(tup_db_create_link(nle->tupid, cmd_id) < 0)
-			return -1;
-	}
-
 	out_id = tup_db_create_node_part(dt, output, -1, TUP_NODE_FILE,
 					 TUP_FLAGS_MODIFY, &node_created);
 	if(out_id < 0)
@@ -555,11 +539,47 @@ static int do_rule(struct rule *r, struct name_list *nl, tupid_t dt)
 	if(node_created)
 		if(tup_db_set_dependent_dir_flags(dt, TUP_FLAGS_CREATE) < 0)
 			return -1;
+
+	list_for_each_entry_safe(nle, tmp, &nl->entries, list) {
+		if(nle->tupid == out_id) {
+			fprintf(stderr, "Error: Attemping to use a command's output as its input in dir ID %lli. Output ID %lli is '%s'. Deleting entry from input list\n", dt, out_id, output);
+			nl->totlen -= nle->len;
+			nl->extlesstotlen -= nle->extlesslen;
+			nl->num_entries--;
+			delete_name_list_entry(nle);
+		}
+	}
+
+	cmd = tup_printf(r->command, nl, output);
+	if(!cmd)
+		return -1;
+	cmd_id = create_command_file(dt, cmd);
+	free(cmd);
 	free(output);
+	if(cmd_id < 0)
+		return -1;
 
 	if(tup_db_create_link(cmd_id, out_id) < 0)
 		return -1;
+
+	list_for_each_entry(nle, &nl->entries, list) {
+		printf("Match[%lli]: %s, %s\n", nl->dt, r->input_pattern,
+		       nle->path);
+		if(nle->tupid == out_id) {
+			return -1;
+		} else {
+			if(tup_db_create_link(nle->tupid, cmd_id) < 0)
+				return -1;
+		}
+	}
 	return 0;
+}
+
+static void delete_name_list_entry(struct name_list_entry *nle)
+{
+	list_del(&nle->list);
+	free(nle->path);
+	free(nle);
 }
 
 static char *tup_printf(const char *cmd, struct name_list *nl, const char *o)
