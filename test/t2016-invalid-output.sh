9df89a2fd09b3445e6ab91c727b8f2498d91e6dc
marf <marfey@gmail.com>
2009-02-20 20:49:38 -0500

    Added t2016 - validate an error message in do_rule()

---
+++ test/t2016-invalid-output.sh
@@ -0,0 +1,14 @@
+#! /bin/sh -e
+
+# Verify that if we try to write outside of the directory where the Tupfile is,
+# things explode.
+
+. ../tup.sh
+cat > Tupfile << HERE
+: foo.c |> gcc -c foo.c -o foo.o |> bar/foo.o
+HERE
+mkdir bar
+tup touch bar/hey
+touch foo.c
+tup touch foo.c Tupfile
+update_fail
