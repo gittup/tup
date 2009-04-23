1c0bef2c8ed409ee6f8bec33dc6825076b1f3fd8
marf <marfey@gmail.com>
2009-04-23 18:21:10 -0400

    Added t2031 - make sure ghost nodes don't stick.
    I figured this would probably already work, since command nodes are re-generated when executing anyway, but it doesn't hurt to check.

---
+++ test/t2031-ghost-node4.sh
@@ -0,0 +1,35 @@
+#! /bin/sh -e
+
+# Make sure a ghost node from a previous command doesn't affect a new command
+# that doesn't rely on its ghostly past.
+
+. ../tup.sh
+cat > ok.sh << HERE
+if [ -f ghost ]; then cat ghost; else echo nofile; fi
+HERE
+cat > Tupfile << HERE
+: |> ./ok.sh > %o |> output.txt
+HERE
+chmod +x ok.sh
+tup touch ok.sh Tupfile
+update
+echo nofile | diff output.txt -
+tup_dep_exist . ghost . './ok.sh > output.txt'
+
+# Change ok.sh so it doesn't try to read from ghost, and make sure the
+# dependency is gone.
+cat > ok.sh << HERE
+echo nofile
+HERE
+tup touch ok.sh
+update
+tup_dep_no_exist . ghost . './ok.sh > output.txt'
+
+# Just as a double-check of sorts - actually create the ghost node and update,
+# after deleting output.txt from behind tup's back. The output.txt file
+# shouldn't be re-created (as it would be in t2028).
+echo 'hey' > ghost
+tup touch ghost
+rm -f output.txt
+update
+check_not_exist output.txt
