d83b5b3b9a88cd65cacc0b8436af36c6ab45daad
marf <marfey@gmail.com>
2009-05-30 17:17:38 -0400

    Added t2051 - make sure you can't include from a bin.

---
+++ test/t2051-include-bin.sh
@@ -0,0 +1,13 @@
+#! /bin/sh -e
+
+# Make sure we can't try to include files from a bin, since those are always
+# generated.
+
+. ../tup.sh
+
+cat > Tupfile << HERE
+: |> echo "var=foo" > %o |> inc [includes]
+include [includes]
+HERE
+tup touch Tupfile
+parse_fail "Shouldn't be able to include files specified in a bin"
