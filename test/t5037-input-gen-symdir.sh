d6bad926244c548d874d2758bd683552305f97e0
marf <marfey@gmail.com>
2009-05-30 16:50:51 -0400

    Added t5037 - can't use generated symlinks in input paths.
    At least, not yet. Sounds like something that might be useful though.

---
+++ test/t5037-input-gen-symdir.sh
@@ -0,0 +1,22 @@
+#! /bin/sh -e
+
+# Make sure we can't use input from a generated symlink to a directory. This
+# would be problematic since we don't know at parse time that an output file
+# will necessarily be a symlink to a directory, so we can't figure out which
+# files to use as input.
+
+. ../tup.sh
+
+tmkdir arch-x86
+cat > Tupfile << HERE
+: |> rm -f %o; ln -s arch-x86 %o |> arch
+HERE
+tup touch Tupfile arch-x86/foo.c
+update
+
+cat > Tupfile << HERE
+: |> rm -f %o; ln -s arch-x86 %o |> arch
+: arch/*.c |> gcc -c %f -o %o |> %B.o
+HERE
+tup touch Tupfile
+parse_fail "Shouldn't be able to use a generated symlink in input specification."
