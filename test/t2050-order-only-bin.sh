52b3dfd6a962436d6ac200d3d69f71ac79854bf3
marf <marfey@gmail.com>
2009-05-30 17:14:18 -0400

    Added t2050 - make sure bins in order-only prereqs work.
    And they do! W00t.

---
+++ test/t2050-order-only-bin.sh
@@ -0,0 +1,14 @@
+#! /bin/sh -e
+
+# Try order-only-ish prerequisites. Now with bins! (copied from t2011)
+
+. ../tup.sh
+cat > Tupfile << HERE
+: |> echo blah > %o |> foo.h [headers]
+: foreach *.c | [headers] |> echo gcc -c %f -o %o |> %F.o
+HERE
+
+tup touch Tupfile foo.c bar.c
+tup parse
+tup_dep_exist . foo.h . "echo gcc -c foo.c -o foo.o"
+tup_dep_exist . foo.h . "echo gcc -c bar.c -o bar.o"
