9cd04308ad64e25ed9383db776b9e9518f5d0fec
marf <marfey@gmail.com>
2009-07-30 20:10:45 -0400

    Added t2059 - make sure dependent directories are parsed.
    Had a similar issue with the updater when a failed node wasn't re-executed since it wasn't put in modify. Since the create phase is only committed if they all succeed, this currently isn't an issue. However, it is nice to have the test since if I change the create phase in the future, this could break.

---
+++ test/t2059-failed-tupfile-dep.sh
@@ -0,0 +1,27 @@
+#! /bin/sh -e
+
+# Make sure a dependent Tupfile that fails still gets re-parsed.
+
+. ../tup.sh
+tmkdir foo
+tmkdir bar
+cat > foo/Tupfile << HERE
+: |> echo yo > %o |> yo.h
+HERE
+cat > bar/Tupfile << HERE
+: ../foo/*.txt |> cp %f %o |> output
+: ../foo/*.c |> cp %f %o |> output
+HERE
+tup touch foo/Tupfile bar/Tupfile foo/ok.txt
+tup upd
+
+# Change yo.h to yo.c so the second rule in bar/Tupfile is triggered to also
+# hit the same output file (thus causing an error).
+cat > foo/Tupfile << HERE
+: |> echo yo > %o |> yo.c
+HERE
+tup touch foo/Tupfile
+update_fail
+
+# An update should again try to parse bar and fail again.
+update_fail
