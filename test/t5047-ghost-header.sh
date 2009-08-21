1f990b1128773861e62f30af1b4944686b6acf55
marf <marfey@gmail.com>
2009-08-20 20:50:48 -0400

    Added t5047 - make sure a removed header becomes a ghost.
    This already worked, but I wasn't sure if it would, so I added it.

---
+++ test/t5047-ghost-header.sh
@@ -0,0 +1,28 @@
+#! /bin/sh -e
+
+# Make sure that if we remove a header so the compilation falls through to the
+# next header, the first one becomes a ghost (the dependency should still be
+# there).
+. ../tup.sh
+
+tmkdir foo
+tmkdir bar
+echo '#define FOO 3' > foo/me.h
+echo '#define FOO 5' > bar/me.h
+echo '#include "me.h"' > ok.c
+
+cat > Tupfile << HERE
+: ok.c |> gcc -c %f -o %o -Ifoo -Ibar |> ok.o
+HERE
+tup touch foo/me.h bar/me.h ok.c Tupfile
+update
+
+tup_dep_exist foo me.h . 'gcc -c ok.c -o ok.o -Ifoo -Ibar'
+tup_dep_no_exist bar me.h . 'gcc -c ok.c -o ok.o -Ifoo -Ibar'
+
+rm foo/me.h
+tup rm foo/me.h
+update
+
+tup_dep_exist foo me.h . 'gcc -c ok.c -o ok.o -Ifoo -Ibar'
+tup_dep_exist bar me.h . 'gcc -c ok.c -o ok.o -Ifoo -Ibar'
