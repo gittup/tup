1347670e7d373fb045fde540bcca322c1b12e125
marf <marfey@gmail.com>
2009-08-20 21:42:02 -0400

    Added t5048 - directory moving test with dependency.
    This is actually a use-case to test the need for tup_db_modify_deleted_deps(). When I tried commenting out the function call in process_delete_nodes(), all tests still passed. I assume this is because that functionality is largely already handled by tup_del_id(). In fact, it may be necessary to move all deleted nodes to be processed through tup_del_id() in order to catch all the cases.

---
+++ test/t5048-move-dir-out.sh
@@ -0,0 +1,22 @@
+#! /bin/sh -e
+
+# Try to move a directory out of tup, and make sure dependent files are still
+# compiled.
+
+. ../tup.sh
+mkdir real
+cd real
+tup init
+tmkdir sub
+echo "#define FOO 3" > sub/foo.h
+echo '#include "foo.h"' > foo.c
+echo ': foreach *.c |> gcc -c %f -o %o -Isub |> %B.o' > Tupfile
+tup touch Tupfile foo.c sub/foo.h
+update
+
+mv sub ..
+tup rm sub
+update_fail
+
+tup touch foo.h
+update
