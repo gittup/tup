783e9aa2fc76f99c6357eaf620d54aa998e73132
marf <marfey@gmail.com>
2009-12-05 21:08:13 -0500

    Remove useless old doc information.

--- docs/include-test/run-tests.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536; do
-	./run-test.sh $i
-	x=`(time -p make > /dev/null) 2>&1 | grep real | sed 's/real //'`
-	echo "$i $x"
-done
