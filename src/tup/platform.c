b09752a6084e7e6dd68a056513d351145aa61b88
marf <marfey@gmail.com>
2010-03-18 22:58:30 -0400

    Add solaris platform.

--- src/tup/platform.c
+++ src/tup/platform.c
@@ -2,6 +2,8 @@
 
 #ifdef __linux__
 const char *tup_platform = "linux";
+#elif __sun__
+const char *tup_platform = "solaris";
 #else
 #error Unsupported platform. Please add support in tup/platform.c
 #endif
