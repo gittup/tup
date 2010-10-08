868c2dff56268b89f8cf5d20d442d11bcd1a59d3
marf <marfey@gmail.com>
2010-10-07 21:43:28 -0400

    win32: Wrap vfprintf as well.
    Now all output is just flushed, so you can actually read things as they happen in the order that they happen.

--- src/compat/win32/printf.c
+++ src/compat/win32/printf.c
@@ -4,10 +4,21 @@
 int __wrap___mingw_vprintf(const char *format, va_list ap);
 int __real___mingw_vprintf(const char *format, va_list ap);
 
+int __wrap___mingw_vfprintf(FILE *stream, const char *format, va_list ap);
+int __real___mingw_vfprintf(FILE *stream, const char *format, va_list ap);
+
 int __wrap___mingw_vprintf(const char *format, va_list ap)
 {
 	int rc;
 	rc = __real___mingw_vprintf(format, ap);
 	fflush(stdout);
+	return rc;
+}
+
+int __wrap___mingw_vfprintf(FILE *stream, const char *format, va_list ap)
+{
+	int rc;
+	rc = __real___mingw_vfprintf(stream, format, ap);
+	fflush(stream);
 	return rc;
 }
