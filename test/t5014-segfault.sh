3071d4c109e036a5928f34433eb4a50c5734c320
Ricardo M. Correia <rcorreia@wizy.org>
2010-11-14 05:50:38 +0100

    Fix test t5014-segfault failure due to mismatched segfault message.
    It seems that at least under Ubuntu 64-bit, when a program segfaults it prints
    "Segmentation fault (core dumped)".
    
    Since the test was expecting "signal 11 (Segmentation fault)", it caused a
    failure. The test was fixed to search for just "Segmentation fault".

--- test/t5014-segfault.sh
+++ test/t5014-segfault.sh
@@ -17,12 +17,12 @@ int main(void)
 }
 HERE
 tup touch segfault.c Tupfile
-update_fail_msg "signal 11 (Segmentation fault)"
+update_fail_msg "Segmentation fault"
 tup_dep_exist . segfault.c . 'gcc segfault.c -o tup_t5014_segfault'
 tup_dep_exist . 'gcc segfault.c -o tup_t5014_segfault' . tup_t5014_segfault
 tup_dep_exist . tup_t5014_segfault . './tup_t5014_segfault'
 
 # Make sure the command runs and fails again.
-update_fail_msg "signal 11 (Segmentation fault)"
+update_fail_msg "Segmentation fault"
 
 eotup
