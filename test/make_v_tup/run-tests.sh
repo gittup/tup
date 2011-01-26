3bd38f62648e0ff87a8a4e392e8c1b727903d70b
Michael Haggerty <mhagger@alum.mit.edu>
2011-01-26 06:12:28 +0100

    If max_user_watches is wrong, show the command needed to adjust it.
    This is a breadcrumb for the next person who comes along, and also
    conveniently copy-pastable into the command line.

--- test/make_v_tup/run-tests.sh
+++ test/make_v_tup/run-tests.sh
@@ -3,5 +3,7 @@ if cat /proc/sys/fs/inotify/max_user_watches | grep 65536 > /dev/null; then
 	for i in 1 10 100 1000 10000 100000; do ./run-test.sh -n $i > out-$i.txt 2>&1; done
 else
 	echo "Please set max_user_watches to 65536"
+	echo "This can be done by running (as root):"
+	echo "    echo 65536 >/proc/sys/fs/inotify/max_user_watches"
 	exit 1;
 fi
