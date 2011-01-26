099f375c4e627ed623a652e22e9786fd5d76bd8d
Michael Haggerty <mhagger@alum.mit.edu>
2011-01-26 10:08:29 +0100

    Change path_names from a hash into an array.

--- test/make_v_tup/gen-test-case.pl
+++ test/make_v_tup/gen-test-case.pl
@@ -2,7 +2,7 @@
 
 use strict;
 
-my ($num_files, $num_deps, %path_names, $x, $y, %dir_names, %mains, @paths, @pathcounts);
+my ($num_files, $num_deps, @path_names, $x, $y, %dir_names, %mains, @paths, @pathcounts);
 my @sample_paths = ("usr", "src", "linux", "mozilla", "marf", "tup", "test", "drivers", "include", "sound");
 
 $paths[0] = "";
@@ -41,15 +41,15 @@ open FILE, ">ttup/Tuprules.tup" or die "Can't open ttup/Tuprules.tup for write.\
 print FILE "TEST_TOP = \$(TUP_CWD)\n";
 close FILE;
 for($x=0; $x<$num_files; $x++) {
-	$path_names{$x} = &generate_path($x);
-	if($path_names{$x} ne "") {
-		system("mkdir -p tmake/$path_names{$x}");
-		system("mkdir -p ttup/$path_names{$x}");
+	$path_names[$x] = &generate_path($x);
+	if($path_names[$x] ne "") {
+		system("mkdir -p tmake/$path_names[$x]");
+		system("mkdir -p ttup/$path_names[$x]");
 	}
-	if($dir_names{$path_names{$x}} != 1) {
-		$dir_names{$path_names{$x}} = 1;
+	if($dir_names{$path_names[$x]} != 1) {
+		$dir_names{$path_names[$x]} = 1;
 		$mains{$x} = 1;
-		system("cp ../testTupfile.tup ttup/$path_names{$x}/Tupfile");
+		system("cp ../testTupfile.tup ttup/$path_names[$x]/Tupfile");
 	}
 }
 
@@ -60,25 +60,25 @@ print MAKEFILE "objs :=\n";
 print MAKEFILE "progs :=\n";
 
 for($x=0; $x<$num_files; $x++) {
-	print MAKEFILE "objs += $path_names{$x}$x.o\n";
-	print MAKEFILE "progs += $path_names{$x}prog\n";
-	print MAKEFILE "$path_names{$x}prog: $path_names{$x}$x.o\n";
-	open FILE, ">ttup/$path_names{$x}$x.c" or die "Can't open ttup/$path_names{$x}$x.c for write\n";
+	print MAKEFILE "objs += $path_names[$x]$x.o\n";
+	print MAKEFILE "progs += $path_names[$x]prog\n";
+	print MAKEFILE "$path_names[$x]prog: $path_names[$x]$x.o\n";
+	open FILE, ">ttup/$path_names[$x]$x.c" or die "Can't open ttup/$path_names[$x]$x.c for write\n";
 	for($y=0; $y<$num_deps; $y++) {
 		my ($tmp);
 		$tmp = ($x + $y) % $num_files;
-		print FILE "#include \"$path_names{$tmp}$tmp.h\"\n";
+		print FILE "#include \"$path_names[$tmp]$tmp.h\"\n";
 	}
 	print FILE "void func_$x(void) {}\n";
 	if($mains{$x}) {
 		print FILE "int main(void) {return 0;}\n";
 	}
 	close FILE;
-	open FILE, ">ttup/$path_names{$x}$x.h" or die "Can't open ttup/$path_names{$x}$x.h for write\n";
+	open FILE, ">ttup/$path_names[$x]$x.h" or die "Can't open ttup/$path_names[$x]$x.h for write\n";
 	print FILE "void func_$x(void);\n";
 	close FILE;
-	system("cp ttup/$path_names{$x}$x.c tmake/$path_names{$x}$x.c");
-	system("cp ttup/$path_names{$x}$x.h tmake/$path_names{$x}$x.h");
+	system("cp ttup/$path_names[$x]$x.c tmake/$path_names[$x]$x.c");
+	system("cp ttup/$path_names[$x]$x.h tmake/$path_names[$x]$x.h");
 }
 
 print MAKEFILE "progs := \$(sort \$(progs))\n";
