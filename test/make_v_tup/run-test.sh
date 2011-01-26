467df216076fb56e310deb5aa5a9b4ce2d7c8bd9
Michael Haggerty <mhagger@alum.mit.edu>
2011-01-26 06:30:41 +0100

    Extract a function benchmark().
    Besides DNRY, using a single function to benchmark both tools makes it
    obvious that the tools are given the same tasks.

--- test/make_v_tup/run-test.sh
+++ test/make_v_tup/run-test.sh
@@ -6,74 +6,47 @@ niter=3
 ../gen-test-case.pl "$@" || exit 1
 sync
 
-tool="make"
-initialize=":"
-start=":"
-update="make -rR > /dev/null"
-finish=":"
-cd t$tool
-find . -type f | while read i; do cat $i > /dev/null; done
-eval "$initialize"
-echo "$tool: initial"
-eval "$start"
-time -p eval "$update"
-sync
-cfile=`find . -name 0.c`;
-hfile=`find . -name 0.h`;
+# Run the full gamut of tests for one of the tools:
+benchmark ()
+{
+	tool="$1"
+	initialize="$2"
+	start="$3"
+	update="$4"
+	finish="$5"
 
-echo "$tool: 0.c touched"
-for i in `seq 1 $niter`; do
-	sleep 1; touch $cfile
+	cd t$tool
+	find . -type f | while read i; do cat $i > /dev/null; done
+	eval "$initialize"
+	echo "$tool: initial"
+	eval "$start"
 	time -p eval "$update"
-done
+	sync
+	cfile=`find . -name 0.c`;
+	hfile=`find . -name 0.h`;
 
-echo "$tool: 0.h touched"
-for i in `seq 1 $niter`; do
-	sleep 1; touch $hfile
-	time -p eval "$update"
-done
+	echo "$tool: 0.c touched"
+	for i in `seq 1 $niter`; do
+		sleep 1; touch $cfile
+		time -p eval "$update"
+	done
 
-echo "$tool: nothing"
-for i in `seq 1 $niter`; do
-	time -p eval "$update"
-done
+	echo "$tool: 0.h touched"
+	for i in `seq 1 $niter`; do
+		sleep 1; touch $hfile
+		time -p eval "$update"
+	done
 
-eval "$finish"
-cd ..
+	echo "$tool: nothing"
+	for i in `seq 1 $niter`; do
+		time -p eval "$update"
+	done
 
-tool="tup"
-initialize="tup init --force > /dev/null"
-start="tup monitor"
-update="tup upd > /dev/null"
-finish="tup stop"
-cd t$tool
-find . -type f | while read i; do cat $i > /dev/null; done
-eval "$initialize"
-echo "$tool: initial"
-eval "$start"
-time -p eval "$update"
-sync
-cfile=`find . -name 0.c`;
-hfile=`find . -name 0.h`;
+	eval "$finish"
+	cd ..
+}
 
-echo "$tool: 0.c touched"
-for i in `seq 1 $niter`; do
-	sleep 1; touch $cfile
-	time -p eval "$update"
-done
-
-echo "$tool: 0.h touched"
-for i in `seq 1 $niter`; do
-	sleep 1; touch $hfile
-	time -p eval "$update"
-done
-
-echo "$tool: nothing"
-for i in `seq 1 $niter`; do
-	time -p eval "$update"
-done
-
-eval "$finish"
-cd ..
+benchmark "make" ":" ":" "make -rR > /dev/null" ":"
+benchmark "tup" "tup init --force > /dev/null" "tup monitor" "tup upd > /dev/null" "tup stop"
 
 #diff -r tmake ttup | grep -v Makefile | grep -v build | grep -v '\.d$' | grep -v '\.tup'
