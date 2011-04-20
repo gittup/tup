4579d588661b1c6bd201e234c5a3358f251158b3
marf <marfey@gmail.com>
2011-04-19 22:40:51 -0400

    Added t7041: Allow autoupdate via command line option.
    This allows the monitor to be controlled both by command-line options as well as config options, similar to the updater. Also, any extra options passed to the monitor (such as -jN) will be given to the updater during an autoupdate.

---
+++ test/t7041-autoupdate4.sh
@@ -0,0 +1,46 @@
+#! /bin/sh -e
+
+# Try autoupdate with the flag instead of the config option.
+
+. ./tup.sh
+check_monitor_supported
+tup monitor --autoupdate
+cat > Tupfile << HERE
+: foreach *.txt |> (echo '<html>'; cat %f; echo '</html>') > %o |> %B.html
+HERE
+
+echo "This is the index" > index.txt
+echo "Another page" > page.txt
+tup flush
+
+check_exist index.html page.html
+cat << HERE | diff index.html -
+<html>
+This is the index
+</html>
+HERE
+
+# Change a file and see that it gets updated
+echo "Updated index" > index.txt
+tup flush
+cat << HERE | diff index.html -
+<html>
+Updated index
+</html>
+HERE
+
+# Add a new file and see it gets updated
+echo "New file" > new.txt
+tup flush
+cat << HERE | diff new.html -
+<html>
+New file
+</html>
+HERE
+
+# Remove a file and see that its dependent file is removed
+rm page.txt
+tup flush
+check_not_exist page.html
+
+eotup
