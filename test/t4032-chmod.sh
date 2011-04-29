c59de327145da7b5bbafe917edecc381e2e61ded
marf <marfey@gmail.com>
2011-04-28 23:36:58 -0400

    Added t4032 - chmod test, similar to chown.

---
+++ test/t4032-chmod.sh
@@ -0,0 +1,19 @@
+#! /bin/sh -e
+
+# Try chmod.
+
+. ./tup.sh
+
+cat > Tupfile << HERE
+: |> touch %o; chmod 664 %o |> test1
+HERE
+tup touch Tupfile
+update
+
+cat > Tupfile << HERE
+: |> chmod 664 test2 |>
+HERE
+tup touch Tupfile test2
+update_fail_msg "test2.*Operation not permitted"
+
+eotup
