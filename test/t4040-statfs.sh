47581ab4e4157cb9356eb310ea0ff7d22c330afa
marf <marfey@gmail.com>
2011-05-10 00:27:58 -0400

    Added t4040 - statfs test.
    This tests the statfs() call, which was fixed in 4f210981.

---
+++ test/t4040-statfs.sh
@@ -0,0 +1,29 @@
+#! /bin/sh -e
+
+# Try statfs
+
+. ./tup.sh
+
+cat > ok.c << HERE
+#include <sys/vfs.h>
+int main(void)
+{
+	struct statfs buf;
+	if(statfs("foo.txt", &buf) < 0) {
+		perror("statfs");
+		return 1;
+	}
+	return 0;
+}
+HERE
+
+cat > Tupfile << HERE
+: ok.c |> gcc %f -o %o |> ok
+: ok |> ./%f |>
+HERE
+tup touch ok.c foo.txt Tupfile
+update
+
+tup_dep_exist . foo.txt . ./ok
+
+eotup
