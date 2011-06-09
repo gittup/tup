936d89bb5f2d07b1f9110e6d8f3b8832cfd31047
marf <marfey@gmail.com>
2011-06-08 21:21:45 -0400

    test.sh: Add --keep-going flag.
    This helps testing on new platforms where many of the tests may be failing, so you can run them all and just do 'ls' to see the directories for all the failed tests.

--- test/test.sh
+++ test/test.sh
@@ -3,13 +3,25 @@
 # Use as:
 # To run a single test: ./test.sh t0000-init.sh
 # To run all tests: ./test.sh
+# To run all tests without stopping on errors: ./test.sh --keep-going
 #
 # You can also invoke a test case directly (as in: ./t0000-init.sh), but if it
 # fails you may not always get an error message. You can still tell if the test
 # failed by checking the return code (non-zero), and the tuptesttmp directory
 # for that test will remain for manual inspection.
 
-files=$@
+keep_going=0
+while [ $# -gt 0 ]; do
+	if [ "$1" = "-k" ]; then
+		keep_going=1
+	elif [ "$1" = "--keep-going" ]; then
+		keep_going=1
+	else
+		files="$files $1"
+	fi
+	shift
+done
+
 if [ "$files" = "" ]; then
 	files="t[0-9]*.sh"
 fi
@@ -20,6 +32,8 @@ for i in $files; do
 		:
 	else
 		echo "[31m *** $i failed[0m"
-		exit 1
+		if [ "$keep_going" = "0" ]; then
+			exit 1
+		fi
 	fi
 done
