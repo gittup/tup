7811a5f8a3c22935d5a662af8daeae1a40ef7b1c
marf <marfey@gmail.com>
2011-06-26 13:07:11 -0400

    t4038: Add specific strip flags for OSX.
    Similar to the change for t4037, we need '-u -r' flags for strip on OSX.

--- test/t4038-strip2.sh
+++ test/t4038-strip2.sh
@@ -15,8 +15,12 @@ cat > sub/bar.c << HERE
 void bar(void) {}
 HERE
 cat > sub/Tupfile << HERE
+# Similar to t4037 - add specific flags for OSX
+ifeq (@(TUP_PLATFORM),macosx)
+stripflags = -u -r
+endif
 : foreach *.c |> gcc -c %f -o %o |> %B.o
-: *.o |> ar cr %o %f && strip %o |> libfoo.a
+: *.o |> ar cr %o %f && strip \$(stripflags) %o |> libfoo.a
 HERE
 tup touch sub/foo.c sub/bar.c sub/Tupfile
 update
