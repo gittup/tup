2ebc8e21e18e47721de2d309faaa478cffc17cf6
marf <marfey@gmail.com>
2011-06-26 12:54:31 -0400

    t4037: Add specific strip flags for OSX.
    Just plain 'strip' doesn't work on OSX - we have to pass in '-u -r' in order for it to complete successfully.

--- test/t4037-strip.sh
+++ test/t4037-strip.sh
@@ -14,8 +14,17 @@ cat > bar.c << HERE
 void bar(void) {}
 HERE
 cat > Tupfile << HERE
+# Unless we pass '-u -r' to strip, OSX fails with: symbols referenced by
+# relocation entries that can't be stripped in ...
+# _bar.eh
+# _bar
+# EH_frame1
+ifeq (@(TUP_PLATFORM),macosx)
+stripflags = -u -r
+endif
+
 : foreach *.c |> gcc -c %f -o %o |> %B.o
-: *.o |> ar cr %o %f && strip %o |> libfoo.a
+: *.o |> ar cr %o %f && strip \$(stripflags) %o |> libfoo.a
 HERE
 tup touch foo.c bar.c Tupfile
 update
