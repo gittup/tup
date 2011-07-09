38d6a5e68d35ebd884e9ac13191a6f3b2ec72370
marf <marfey@gmail.com>
2011-07-09 10:21:32 -0400

    fslurp: Add perror calls.
    I think originally this was done so the client could call perror if they wanted to, but since no clients do that, they should be done here.

--- src/tup/fslurp.c
+++ src/tup/fslurp.c
@@ -1,4 +1,5 @@
 #include "fslurp.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
@@ -28,19 +29,23 @@ static int do_slurp(int fd, struct buf *b, int extra)
 	int rc;
 
 	if(fstat(fd, &st) < 0) {
+		perror("fstat");
 		return -1;
 	}
 
 	tmp = malloc(st.st_size + extra);
 	if(!tmp) {
+		perror("malloc");
 		return -1;
 	}
 
 	rc = read(fd, tmp, st.st_size);
 	if(rc < 0) {
+		perror("read");
 		goto err_out;
 	}
 	if(rc != st.st_size) {
+		fprintf(stderr, "tup error: read %i bytes, but expected %lli bytes\n", rc, st.st_size);
 		errno = EIO;
 		goto err_out;
 	}
