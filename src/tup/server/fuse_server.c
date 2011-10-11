fbfffe4201929959ed6f37cee82b0a69df9c2e5e
marf <marfey@gmail.com>
2011-10-10 20:28:59 -0400

    osx: Revert to original fuse unmounting.
    Calling unmount() in OSX seems to occasionally cause a hang. The original method of calling fuse_exit() directly followed by attempting to open the mountpoint works more consistently.

--- src/tup/server/fuse_server.c
+++ src/tup/server/fuse_server.c
@@ -58,9 +58,10 @@ static void *fuse_thread(void *arg)
 }
 
 #if defined(__linux__)
-static int os_unmount(void)
+static int os_unmount(int force)
 {
 	int rc;
+	if(force) {}
 	rc = system("fusermount -u -z " TUP_MNT);
 	if(rc == -1) {
 		perror("system");
@@ -68,11 +69,22 @@ static int os_unmount(void)
 	return rc;
 }
 #elif defined(__APPLE__)
-static int os_unmount(void)
+static int os_unmount(int force)
 {
-	if(unmount(TUP_MNT, MNT_FORCE) < 0) {
-		perror("unmount");
-		return -1;
+	if(force) {
+		/* TODO: This seems to cause OSX to occasionally hang - race in fuse4x? */
+		if(unmount(TUP_MNT, MNT_FORCE) < 0) {
+			perror("unmount");
+			return -1;
+		}
+	} else {
+		int fd;
+		fuse_exit(fs.fuse);
+		fd = openat(tup_top_fd(), TUP_MNT, O_RDONLY);
+		if(fd >= 0) {
+			fprintf(stderr, "tup internal error: Expected open(%s) to fail on FUSE filesystem\n", TUP_MNT);
+			return -1;
+		}
 	}
 	return 0;
 }
@@ -80,7 +92,7 @@ static int os_unmount(void)
 #error Unsupported platform. Please add unmounting code to fuse_server.c
 #endif
 
-static int tup_unmount(void)
+static int tup_unmount(int force)
 {
 	int rc;
 
@@ -88,7 +100,7 @@ static int tup_unmount(void)
 		perror("fchdir");
 		rc = -2;
 	} else {
-		rc = os_unmount();
+		rc = os_unmount(force);
 	}
 	if(rc != 0) {
 		fprintf(stderr, "tup error: Unable to unmount the fuse file-system on .tup/mnt (return code = %i). You may have to unmount this manually as root: umount -f .tup/mnt\n", rc);
@@ -148,7 +160,7 @@ int server_init(enum server_mode mode, struct tupid_entries *delete_root)
 				/* This directory is still mounted, let's try
 				 * to umount it
 				 */
-				if(tup_unmount() < 0)
+				if(tup_unmount(1) < 0)
 					return -1;
 			}
 		} else {
@@ -247,7 +259,7 @@ int server_quit(void)
 		return 0;
 	close(null_fd);
 
-	if(tup_unmount() < 0)
+	if(tup_unmount(0) < 0)
 		return -1;
 	pthread_join(fs.pid, NULL);
 	fs.fuse = NULL;
