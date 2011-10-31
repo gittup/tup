gittup/tup

commit 66306c931d569d3a8f6a86d664a184a84923ae99

display: Show failed sub-processes in red.
This requires saving off the dependency-checking errors to a temporary file, so that we know before we print those error messages whether or not it should have a red or blue banner.

Note that not all errors are sent to the file - for example, if there is a database error, this just gets written to stdout. These errors are typically not associated with a specific sub-process (like an unspecified output error), but are rather indications of tup bugs. Therefore they should be displayed as soon as possible.

src/tup/config.c
@@ -162,13 +162,16 @@ int display_output(int fd, int iserr, const char *name, int display_name)
 				break;
 			if(!displayed) {
 				displayed = 1;
-				if(iserr) {
+				if(iserr == 2) {
+					fprintf(stderr, " *** tup errors ***\n");
+				}
+				if(iserr == 1) {
 					if(display_name) {
 						fprintf(stderr, " *** tup: stderr from command '%s%s%s%s' ***\n", color_type(TUP_NODE_CMD), color_append_normal(), name, color_end());
 					} else {
 						fprintf(stderr, " *** tup: stderr ***\n");
 					}
-				} else {
+				} else if(iserr == 0) {
 					if(display_name) {
 						printf(" -- tup: stdout from command '%s%s%s%s' --\n", color_type(TUP_NODE_CMD), color_append_normal(), name, color_end());
 					} else {

src/tup/server/fuse_server.c
@@ -347,7 +347,7 @@ static int exec_internal(struct server *s, const char *cmd,
 
 	snprintf(buf, sizeof(buf), ".tup/tmp/errors-%i", s->id);
 	buf[sizeof(buf)-1] = 0;
-	s->error_fd = openat(tup_top_fd(), buf, O_RDONLY);
+	s->error_fd = openat(tup_top_fd(), buf, O_RDWR);
 	if(s->error_fd < 0) {
 		perror(buf);
 		fprintf(stderr, "tup error: Unable to open sub-process errors file.\n");

src/tup/updater.c
@@ -44,7 +44,7 @@ static int update(struct node *n);
 static void tup_show_message(const char *s);
 static void tup_main_progress(const char *s);
 static void start_progress(int total);
-static void show_progress(struct tup_entry *tent);
+static void show_progress(struct tup_entry *tent, int is_error);
 static void show_active(int active);
 
 static int do_keep_going;
@@ -285,7 +285,7 @@ static int delete_files(struct graph *g)
 
 			/* Only delete if the file wasn't modified (t6031) */
 			if(do_delete) {
-				show_progress(tent);
+				show_progress(tent, 0);
 				if(delete_file(tent->dt, tent->name.s) < 0)
 					goto out_err;
 			}
@@ -303,10 +303,10 @@ static int delete_files(struct graph *g)
 	LIST_FOREACH(tent, entrylist, list) {
 		if(tup_db_set_type(tent, TUP_NODE_FILE) < 0)
 			goto out_err;
-		show_progress(tent);
+		show_progress(tent, 0);
 	}
 	if(g->delete_count) {
-		show_progress(NULL);
+		show_progress(NULL, 0);
 	}
 	rc = 0;
 out_err:
@@ -758,7 +758,7 @@ keep_going:
 		}
 		goto out;
 	}
-	show_progress(NULL);
+	show_progress(NULL, 0);
 	rc = 0;
 out:
 	/* First tell all the threads to quit */
@@ -820,7 +820,7 @@ static void *create_work(void *arg)
 			break;
 
 		if(n->tent->type == TUP_NODE_DIR) {
-			show_progress(n->tent);
+			show_progress(n->tent, 0);
 			if(n->already_used) {
 				printf("Already parsed[%lli]: '%s'\n", n->tnode.tupid, n->tent->name.s);
 				rc = 0;
@@ -929,7 +929,7 @@ static void *todo_work(void *arg)
 			break;
 
 		if(n->tent->type == g->count_flags) {
-			show_progress(n->tent);
+			show_progress(n->tent, 0);
 			tup_db_print(stdout, n->tnode.tupid);
 		}
 
@@ -947,7 +947,7 @@ static int unlink_outputs(int dfd, struct node *n)
 		if(unlinkat(dfd, output->tent->name.s, 0) < 0) {
 			if(errno != ENOENT) {
 				pthread_mutex_lock(&display_mutex);
-				show_progress(n->tent);
+				show_progress(n->tent, 1);
 				perror("unlinkat");
 				fprintf(stderr, "tup error: Unable to unlink previous output file: %s\n", output->tent->name.s);
 				pthread_mutex_unlock(&display_mutex);
@@ -960,25 +960,29 @@ static int unlink_outputs(int dfd, struct node *n)
 
 static int process_output(struct server *s, struct tup_entry *tent)
 {
-	if(display_output(s->output_fd, 0, tent->name.s, 0) < 0)
+	FILE *f;
+	int is_err = 1;
+
+	f = tmpfile();
+	if(!f) {
+		show_progress(tent, 1);
+		perror("tmpfile");
+		fprintf(stderr, "tup error: Unable to open the error log for writing.\n");
 		return -1;
-	if(display_output(s->error_fd, 1, tent->name.s, 0) < 0)
-		return -1;
+	}
 	if(s->exited) {
 		if(s->exit_status == 0) {
-			if(write_files(tent->tnode.tupid, &s->finfo, &warnings, 0) < 0) {
-				fprintf(stderr, " *** Command %lli ran successfully, but tup failed to save the dependencies.\n", tent->tnode.tupid);
-				return -1;
+			if(write_files(f, tent->tnode.tupid, &s->finfo, &warnings, 0) < 0) {
+				fprintf(f, " *** Command ID=%lli ran successfully, but tup failed to save the dependencies.\n", tent->tnode.tupid);
+			} else {
+				/* Hooray! */
+				is_err = 0;
 			}
-
-			/* Hooray! */
-			return 0;
 		} else {
-			fprintf(stderr, " *** Command %lli failed with return value %i\n", tent->tnode.tupid, s->exit_status);
-			if(write_files(tent->tnode.tupid, &s->finfo, &warnings, 1) < 0) {
-				fprintf(stderr, " *** Additionally, command %lli failed to process input dependencies. These should probably be fixed before addressing the command failure.\n", tent->tnode.tupid);
+			fprintf(f, " *** Command ID=%lli failed with return value %i\n", tent->tnode.tupid, s->exit_status);
+			if(write_files(f, tent->tnode.tupid, &s->finfo, &warnings, 1) < 0) {
+				fprintf(f, " *** Additionally, command %lli failed to process input dependencies. These should probably be fixed before addressing the command failure.\n", tent->tnode.tupid);
 			}
-			return -1;
 		}
 	} else if(s->signalled) {
 		int sig = s->exit_sig;
@@ -986,12 +990,25 @@ static int process_output(struct server *s, struct tup_entry *tent)
 
 		if(sig >= 0 && sig < ARRAY_SIZE(signal_err) && signal_err[sig])
 			errmsg = signal_err[sig];
-		fprintf(stderr, " *** Command %lli killed by signal %i (%s)\n", tent->tnode.tupid, sig, errmsg);
-		return -1;
+		fprintf(f, " *** Command ID=%lli killed by signal %i (%s)\n", tent->tnode.tupid, sig, errmsg);
 	} else {
-		fprintf(stderr, "tup internal error: Expected s->exited or s->signalled to be set for command %lli", tent->tnode.tupid);
-		return -1;
+		fprintf(f, "tup internal error: Expected s->exited or s->signalled to be set for command ID=%lli", tent->tnode.tupid);
 	}
+
+	fflush(f);
+	rewind(f);
+
+	show_progress(tent, is_err);
+	if(display_output(s->output_fd, 0, tent->name.s, 0) < 0)
+		return -1;
+	if(display_output(s->error_fd, 1, tent->name.s, 0) < 0)
+		return -1;
+	if(display_output(fileno(f), 2, tent->name.s, 0) < 0)
+		return -1;
+	fclose(f);
+	if(is_err)
+		return -1;
+	return 0;
 }
 
 static int update(struct node *n)
@@ -1008,7 +1025,7 @@ static int update(struct node *n)
 			 * what yet.
 			 */
 			pthread_mutex_lock(&display_mutex);
-			show_progress(n->tent);
+			show_progress(n->tent, 1);
 			fprintf(stderr, "Error: Unknown ^ flag: '%c'\n", *name);
 			pthread_mutex_unlock(&display_mutex);
 			name++;
@@ -1017,7 +1034,7 @@ static int update(struct node *n)
 		while(*name && *name != '^') name++;
 		if(!*name) {
 			pthread_mutex_lock(&display_mutex);
-			show_progress(n->tent);
+			show_progress(n->tent, 1);
 			fprintf(stderr, "Error: Missing ending '^' flag in command %lli: %s\n", n->tnode.tupid, n->tent->name.s);
 			pthread_mutex_unlock(&display_mutex);
 			return -1;
@@ -1029,7 +1046,7 @@ static int update(struct node *n)
 	dfd = tup_entry_open(n->tent->parent);
 	if(dfd < 0) {
 		pthread_mutex_lock(&display_mutex);
-		show_progress(n->tent);
+		show_progress(n->tent, 1);
 		fprintf(stderr, "Error: Unable to open directory for update work.\n");
 		tup_db_print(stderr, n->tent->parent->tnode.tupid);
 		pthread_mutex_unlock(&display_mutex);
@@ -1048,14 +1065,13 @@ static int update(struct node *n)
 	s.error_fd = -1;
 	init_file_info(&s.finfo);
 	if(server_exec(&s, dfd, name, n->tent->parent) < 0) {
-		fprintf(stderr, " *** Command %lli failed: %s\n", n->tnode.tupid, name);
+		fprintf(stderr, " *** Command ID=%lli failed: %s\n", n->tnode.tupid, name);
 		goto err_close_dfd;
 	}
 	close(dfd);
 
 	pthread_mutex_lock(&db_mutex);
 	pthread_mutex_lock(&display_mutex);
-	show_progress(n->tent);
 	rc = process_output(&s, n->tent);
 	pthread_mutex_unlock(&display_mutex);
 	pthread_mutex_unlock(&db_mutex);
@@ -1090,7 +1106,7 @@ static void start_progress(int new_total)
 	total = new_total;
 }
 
-static void show_bar(int node_type)
+static void show_bar(FILE *f, int node_type)
 {
 	if(total) {
 		const int max = 11;
@@ -1100,6 +1116,8 @@ static void show_bar(int node_type)
 		if(is_active) {
 			printf("\r                             \r");
 			is_active = 0;
+			if(f == stderr)
+				fflush(stdout);
 		}
 
 		/* If it's a good enough limit for Final Fantasy VII, it's good
@@ -1110,32 +1128,45 @@ static void show_bar(int node_type)
 		} else {
 			snprintf(buf, sizeof(buf), " %4i/%-4i ", sum, total);
 		}
-		fill = max * sum / total;
+		/* TUP_NODE_ROOT means an error - fill the whole bar so it's
+		 * obvious.
+		 */
+		if(node_type == TUP_NODE_ROOT)
+			fill = max;
+		else
+			fill = max * sum / total;
 
 		if(node_type < 0) {
 			printf("[%s%.*s%s]\n", color_final(), (int)sizeof(buf), buf, color_end());
 		} else {
-			printf("[%s%s%.*s%s%.*s] ", color_type(node_type), color_append_reverse(), fill, buf, color_end(), max-fill, buf+fill);
+			fprintf(f, "[%s%s%.*s%s%.*s] ", color_type(node_type), color_append_reverse(), fill, buf, color_end(), max-fill, buf+fill);
 		}
 	}
 }
 
-static void show_progress(struct tup_entry *tent)
+static void show_progress(struct tup_entry *tent, int is_error)
 {
 	if(tent) {
-		show_bar(tent->type);
-		print_tup_entry(stdout, tent);
-		printf("\n");
+		FILE *f;
+		if(is_error) {
+			f = stderr;
+			show_bar(f, TUP_NODE_ROOT);
+		} else {
+			f = stdout;
+			show_bar(f, tent->type);
+		}
+		print_tup_entry(f, tent);
+		fprintf(f, "\n");
 		sum++;
 	} else {
-		show_bar(-1);
+		show_bar(stdout, -1);
 	}
 }
 
 static void show_active(int active)
 {
 	if(total) {
-		show_bar(TUP_NODE_CMD);
+		show_bar(stdout, TUP_NODE_CMD);
 		printf("Active: %i", active);
 		fflush(stdout);
 		is_active = 1;

src/tup/colors/colors.c
@@ -16,6 +16,12 @@ const char *color_type(int type)
 		return "";
 
 	switch(type) {
+		case TUP_NODE_ROOT:
+			/* Overloaded to mean a node in error (can be re-used
+			 * since TUP_NODE_ROOT is never displayed).
+			 */
+			color = "[31";
+			break;
 		case TUP_NODE_DIR:
 			color = "[33";
 			break;

src/tup/db.c
@@ -3669,6 +3669,7 @@ static int compare_trees(struct tupid_entries *a, struct tupid_entries *b,
 }
 
 struct actual_output_data {
+	FILE *f;
 	tupid_t cmdid;
 	int output_error;
 };
@@ -3683,21 +3684,20 @@ static int extra_output(tupid_t tupid, void *data)
 
 	if(!(aod->output_error & 1)) {
 		aod->output_error |= 1;
-		fprintf(stderr, "tup error: Unspecified output files - A command is writing to files that you didn't specify in the Tupfile. You should add them so tup knows what to expect.\n");
-		fprintf(stderr, " -- Command ID: %lli\n", aod->cmdid);
+		fprintf(aod->f, "tup error: Unspecified output files - A command is writing to files that you didn't specify in the Tupfile. You should add them so tup knows what to expect.\n");
 		/* Return success here so we can display all errant outputs.
 		 * Actual check is in tup_db_check_actual_outputs().
 		 */
 #ifdef _WIN32
 		/* TODO: This can be removed once win32 supports tmp files */
 		tup_db_modify_cmds_by_output(tent->tnode.tupid, NULL);
-		fprintf(stderr, "[35m -- Delete: %s at dir %lli[0m\n",
+		fprintf(aod->f, "[35m -- Delete: %s at dir %lli[0m\n",
 			tent->name.s, tent->dt);
 		delete_file(tent->dt, tent->name.s);
 #endif
 	}
 
-	fprintf(stderr, " -- Unspecified output: %s at dir %lli\n", tent->name.s, tent->dt);
+	fprintf(aod->f, " -- Unspecified output: %s at dir %lli\n", tent->name.s, tent->dt);
 	return 0;
 }
 
@@ -3709,7 +3709,7 @@ static int missing_output(tupid_t tupid, void *data)
 	if(tup_entry_add(tupid, &tent) < 0)
 		return -1;
 
-	fprintf(stderr, "Error: Expected to write to file '%s' from cmd %lli but didn't\n", tent->name.s, aod->cmdid);
+	fprintf(aod->f, "Error: Expected to write to file '%s' from cmd %lli but didn't\n", tent->name.s, aod->cmdid);
 
 	if(!(aod->output_error & 2)) {
 		aod->output_error |= 2;
@@ -3720,10 +3720,12 @@ static int missing_output(tupid_t tupid, void *data)
 	return 0;
 }
 
-int tup_db_check_actual_outputs(tupid_t cmdid, struct tup_entry_head *writehead)
+int tup_db_check_actual_outputs(FILE *f, tupid_t cmdid,
+				struct tup_entry_head *writehead)
 {
 	struct tupid_entries output_root = {NULL};
 	struct actual_output_data aod = {
+		.f = f,
 		.cmdid = cmdid,
 		.output_error = 0,
 	};
@@ -3813,6 +3815,7 @@ int tup_db_write_inputs(tupid_t cmdid, struct tupid_entries *input_root,
 }
 
 struct actual_input_data {
+	FILE *f;
 	tupid_t cmdid;
 	int input_error;
 	struct tupid_entries sticky_root;
@@ -3843,12 +3846,11 @@ static int new_input(tupid_t tupid, void *data)
 		}
 
 		if(!aid->input_error) {
-			fprintf(stderr, "tup error: Missing input dependency - a file was read from, and was not specified as an input link for the command. This is an issue because the file was created from another command, and without the input link the commands may execute out of order. You should add this file as an input, since it is possible this could randomly break in the future.\n");
-			fprintf(stderr, " -- Command ID: %lli\n", aid->cmdid);
+			fprintf(aid->f, "tup error: Missing input dependency - a file was read from, and was not specified as an input link for the command. This is an issue because the file was created from another command, and without the input link the commands may execute out of order. You should add this file as an input, since it is possible this could randomly break in the future.\n");
 		}
 		if(tupid_tree_add(&aid->missing_input_root, tent->tnode.tupid) < 0)
 			return -1;
-		tup_db_print(stderr, tupid);
+		tup_db_print(aid->f, tupid);
 		aid->input_error = 1;
 		/* Return success here so we can display all errant inputs.
 		 * Actual check is in tup_db_check_actual_inputs().
@@ -3900,11 +3902,13 @@ static int del_normal_link(tupid_t tupid, void *data)
 	return 0;
 }
 
-int tup_db_check_actual_inputs(tupid_t cmdid, struct tup_entry_head *readhead)
+int tup_db_check_actual_inputs(FILE *f, tupid_t cmdid,
+			       struct tup_entry_head *readhead)
 {
 	struct tupid_entries normal_root = {NULL};
 	struct tupid_entries sticky_copy = {NULL};
 	struct actual_input_data aid = {
+		.f = f,
 		.cmdid = cmdid,
 		.input_error = 0,
 		.sticky_root = {NULL},

src/tup/db.h
@@ -135,7 +135,9 @@ int tup_db_scan_begin(struct tupid_entries *root);
 int tup_db_scan_end(struct tupid_entries *root);
 
 /* updater operations */
-int tup_db_check_actual_outputs(tupid_t cmdid, struct tup_entry_head *writehead);
-int tup_db_check_actual_inputs(tupid_t cmdid, struct tup_entry_head *readhead);
+int tup_db_check_actual_outputs(FILE *f, tupid_t cmdid,
+				struct tup_entry_head *writehead);
+int tup_db_check_actual_inputs(FILE *f, tupid_t cmdid,
+			       struct tup_entry_head *readhead);
 
 #endif

src/tup/file.c
@@ -23,9 +23,9 @@ static void del_entry(struct file_entry *fent);
 static void check_unlink_list(const struct pel_group *pg,
 			      struct file_entry_head *u_head);
 static void handle_unlink(struct file_info *info);
-static int update_write_info(tupid_t cmdid, struct file_info *info, int *warnings,
-			     struct tup_entry_head *entryhead);
-static int update_read_info(tupid_t cmdid, struct file_info *info,
+static int update_write_info(FILE *f, tupid_t cmdid, struct file_info *info,
+			     int *warnings, struct tup_entry_head *entryhead);
+static int update_read_info(FILE *f, tupid_t cmdid, struct file_info *info,
 			    struct tup_entry_head *entryhead);
 static int add_parser_files_locked(struct file_info *finfo,
 				   struct tupid_entries *root);
@@ -104,7 +104,7 @@ int handle_open_file(enum access_type at, const char *filename,
 	return rc;
 }
 
-int write_files(tupid_t cmdid, struct file_info *info, int *warnings,
+int write_files(FILE *f, tupid_t cmdid, struct file_info *info, int *warnings,
 		int check_only)
 {
 	struct tup_entry_head *entrylist;
@@ -117,7 +117,7 @@ int write_files(tupid_t cmdid, struct file_info *info, int *warnings,
 
 	if(!check_only) {
 		LIST_FOREACH(tmpdir, &info->tmpdir_list, list) {
-			fprintf(stderr, "tup error: Directory '%s' was created, but not subsequently removed. Only temporary directories can be created by commands.\n", tmpdir->dirname);
+			fprintf(f, "tup error: Directory '%s' was created, but not subsequently removed. Only temporary directories can be created by commands.\n", tmpdir->dirname);
 			tmpdir_bork = 1;
 		}
 		if(tmpdir_bork) {
@@ -126,12 +126,12 @@ int write_files(tupid_t cmdid, struct file_info *info, int *warnings,
 		}
 
 		entrylist = tup_entry_get_list();
-		rc1 = update_write_info(cmdid, info, warnings, entrylist);
+		rc1 = update_write_info(f, cmdid, info, warnings, entrylist);
 		tup_entry_release_list();
 	}
 
 	entrylist = tup_entry_get_list();
-	rc2 = update_read_info(cmdid, info, entrylist);
+	rc2 = update_read_info(f, cmdid, info, entrylist);
 	tup_entry_release_list();
 	finfo_unlock(info);
 
@@ -376,8 +376,8 @@ static void handle_unlink(struct file_info *info)
 	}
 }
 
-static int update_write_info(tupid_t cmdid, struct file_info *info, int *warnings,
-			     struct tup_entry_head *entryhead)
+static int update_write_info(FILE *f, tupid_t cmdid, struct file_info *info,
+			     int *warnings, struct tup_entry_head *entryhead)
 {
 	struct file_entry *w;
 	struct file_entry *r;
@@ -402,18 +402,18 @@ static int update_write_info(tupid_t cmdid, struct file_info *info, int *warning
 		}
 
 		if(w->pg.pg_flags & PG_HIDDEN) {
-			fprintf(stderr, "tup warning: Writing to hidden file '%s'\n", w->filename);
+			fprintf(f, "tup warning: Writing to hidden file '%s'\n", w->filename);
 			(*warnings)++;
 			goto out_skip;
 		}
 
 		newdt = find_dir_tupid_dt_pg(w->dt, &w->pg, &pel, 0);
 		if(newdt <= 0) {
-			fprintf(stderr, "tup error: File '%s' was written to, but is not in .tup/db. You probably should specify it as an output\n", w->filename);
+			fprintf(f, "tup error: File '%s' was written to, but is not in .tup/db. You probably should specify it as an output\n", w->filename);
 			return -1;
 		}
 		if(!pel) {
-			fprintf(stderr, "[31mtup internal error: find_dir_tupid_dt_pg() in write_files() didn't get a final pel pointer.[0m\n");
+			fprintf(f, "[31mtup internal error: find_dir_tupid_dt_pg() in write_files() didn't get a final pel pointer.[0m\n");
 			return -1;
 		}
 
@@ -421,7 +421,7 @@ static int update_write_info(tupid_t cmdid, struct file_info *info, int *warning
 			return -1;
 		free(pel);
 		if(!tent) {
-			fprintf(stderr, "tup error: File '%s' was written to, but is not in .tup/db. You probably should specify it as an output\n", w->filename);
+			fprintf(f, "tup error: File '%s' was written to, but is not in .tup/db. You probably should specify it as an output\n", w->filename);
 			write_bork = 1;
 		} else {
 			struct mapping *map;
@@ -449,7 +449,7 @@ out_skip:
 		return -1;
 	}
 
-	if(tup_db_check_actual_outputs(cmdid, entryhead) < 0)
+	if(tup_db_check_actual_outputs(f, cmdid, entryhead) < 0)
 		return -1;
 
 	while(!LIST_EMPTY(&info->mapping_list)) {
@@ -461,7 +461,7 @@ out_skip:
 		if(strcmp(map->tmpname, map->realname) != 0) {
 			if(renameat(tup_top_fd(), map->tmpname, tup_top_fd(), map->realname) < 0) {
 				perror(map->realname);
-				fprintf(stderr, "tup error: Unable to rename temporary file '%s' to destination '%s'\n", map->tmpname, map->realname);
+				fprintf(f, "tup error: Unable to rename temporary file '%s' to destination '%s'\n", map->tmpname, map->realname);
 				write_bork = 1;
 			}
 		}
@@ -479,7 +479,7 @@ out_skip:
 	return 0;
 }
 
-static int update_read_info(tupid_t cmdid, struct file_info *info,
+static int update_read_info(FILE *f, tupid_t cmdid, struct file_info *info,
 			    struct tup_entry_head *entryhead)
 {
 	struct file_entry *r;
@@ -501,7 +501,7 @@ static int update_read_info(tupid_t cmdid, struct file_info *info,
 		del_entry(r);
 	}
 
-	if(tup_db_check_actual_inputs(cmdid, entryhead) < 0)
+	if(tup_db_check_actual_inputs(f, cmdid, entryhead) < 0)
 		return -1;
 	return 0;
 }

src/tup/file.h
@@ -6,6 +6,7 @@
 #include "bsd/queue.h"
 #include "thread_tree.h"
 #include "pel_group.h"
+#include <stdio.h>
 #include <pthread.h>
 
 struct tup_entry;
@@ -53,7 +54,7 @@ int handle_file(enum access_type at, const char *filename, const char *file2,
 int handle_open_file(enum access_type at, const char *filename,
 		     struct file_info *info, tupid_t dt);
 int handle_rename(const char *from, const char *to, struct file_info *info);
-int write_files(tupid_t cmdid, struct file_info *info, int *warnings,
+int write_files(FILE *f, tupid_t cmdid, struct file_info *info, int *warnings,
 		int check_only);
 int add_parser_files(struct file_info *info, struct tupid_entries *root);
 void del_map(struct mapping *map);

commit 2ed1cbc863fe8719d55e60bcf8ad7a7d1e2fdb01

Move close(fd) out of display_output.
Closing it there doesn't make sense since I have to fclose() on some of the streams anyway, which also closes the file descriptor. Trying to close it twice may end up closing a handle in another thread, like the fuse fs.

Fixes a bug introduced in 66306c931.

src/tup/config.c
@@ -202,7 +202,6 @@ int display_output(int fd, int iserr, const char *name, int display_name)
 			}
 			fprintf(out, "%.*s", rc, buf);
 		}
-		close(fd);
 	}
 	return 0;
 }

src/tup/server/fuse_server.c
@@ -430,6 +430,7 @@ int server_run_script(tupid_t tupid, const char *cmdline, char **rules)
 
 	if(display_output(s.error_fd, 1, cmdline, 1) < 0)
 		return -1;
+	close(s.error_fd);
 
 	if(s.exited) {
 		if(s.exit_status == 0) {

src/tup/updater.c
@@ -1012,6 +1012,7 @@ static int process_output(struct server *s, struct tup_entry *tent)
 	show_progress(tent, is_err);
 	if(display_output(s->output_fd, is_err ? 3 : 0, tent->name.s, 0) < 0)
 		return -1;
+	close(s->output_fd);
 	if(display_output(fileno(f), 2, tent->name.s, 0) < 0)
 		return -1;
 	fclose(f);