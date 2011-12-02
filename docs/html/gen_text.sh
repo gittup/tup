2d38ba98beb6fbaaaab83dcbc9e3ec11499df80b
Mike Shal <marfey@gmail.com>
2011-12-02 17:00:30 -0500

    html: Don't use bash-isms in gen_text.sh

--- docs/html/gen_text.sh
+++ docs/html/gen_text.sh
@@ -1,3 +1,2 @@
 #! /bin/sh -e
-
-for j in $(echo $1 | sed 's/\.html//; s/_/ /g; s/index/home/; s/^ex //'); do echo -n ${j:0:1} | tr "[:lower:]" "[:upper:]"; echo -n "${j:1}&nbsp;"; done | sed 's/And&nbsp/and\&nbsp/; s/Vs&nbsp/vs\&nbsp/; s/&nbsp;$//'
+for j in `echo $1 | sed 's/\.html//; s/_/ /g; s/index/home/; s/^ex //'`; do echo $j | awk 'BEGIN{ORS="&nbsp;"} {print toupper(substr($0, 1, 1)) substr($0, 2)}'; done | sed 's/And&nbsp/and\&nbsp/; s/Vs&nbsp/vs\&nbsp/; s/&nbsp;$//'
