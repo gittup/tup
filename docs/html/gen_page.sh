0ca9b63e6dea9b5a7ca83938b000adf985262dbb
Mike Shal <marfey@gmail.com>
2011-12-16 11:47:21 -0500

    html: Add Google Analytics.

--- docs/html/gen_page.sh
+++ docs/html/gen_page.sh
@@ -1,5 +1,10 @@
 #! /bin/sh -e
 
+analytics=0
+if [ $1 = "-a" ]; then
+	analytics=1
+	shift
+fi
 example=0
 if [ $1 = "-x" ]; then
 	example=1
@@ -34,6 +39,25 @@ cat << HERE
 <div style="clear:both;"></div>
 </div>
 <div id="footer">&copy; 2011 Mike Shal. All Rights Reserved.</div>
+HERE
+
+if [ $analytics = "1" ]; then
+cat << HERE
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-171695-12']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+HERE
+fi
+
+cat << HERE
 </body>
 </html>
 HERE
