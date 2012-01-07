275a64c1dd40b0cd6a0bf974ef65943227a0b927
Mike Shal <marfey@gmail.com>
2012-01-07 12:01:21 -0500

    Add copyright info to test cases.

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,21 @@
 #! /bin/sh -e
+# tup - A file-based build system
 #
+# Copyright (C) 2010-2012  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # Use as:
 # To run a single test: ./test.sh t0000-init.sh
 # To run all tests: ./test.sh
