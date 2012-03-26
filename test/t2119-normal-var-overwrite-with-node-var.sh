f397ba5543dd594c29bb54046dfd9a0414a0c2d2
Doug Rosvick <dlr365@gmail.com>
2012-03-26 11:08:49 -0600

    added t2119 - normal variables cannot be converted to %-variables

---
+++ test/t2119-normal-var-overwrite-with-node-var.sh
@@ -0,0 +1,32 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2009-2012  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test that converting a normal var to a node var doesn't work.
+
+. ./tup.sh
+
+cat > Tupfile << HERE
+normal_var = some value
+normal_var %= lib.a
+HERE
+
+tup touch lib.a Tupfile
+
+update_fail_msg "Error setting variable 'normal_var'"
+
+eotup
