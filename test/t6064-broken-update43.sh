d813e0a9d9d4dfef7279bff641757bdd27df3ac5
Mike Shal <marfey@gmail.com>
2012-04-11 23:00:35 -0400

    Added t6064 - fix for 'tup init' inside a hidden path.
    This is mostly to support builds in Jenkins, which by default runs in ~/.jenkins/... The hidden path check was happening before the path before the tup hierarchy was pruned, so it was just ignoring every file. Instead we can move the hidden path check to after the pruning.

---
+++ test/t6064-broken-update43.sh
@@ -0,0 +1,36 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2012  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Make sure that a .path before the tup hierarchy doesn't make every file
+# hidden.
+
+. ./tup.sh
+
+mkdir .tmp
+cd .tmp
+re_init
+
+cat > Tupfile << HERE
+: |> gcc -c foo.c |> foo.o
+HERE
+tup touch Tupfile foo.c
+update
+
+check_exist foo.o
+
+eotup
