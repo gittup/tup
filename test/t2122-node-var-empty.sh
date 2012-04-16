369024b38a086de8d7ea89729cde33d445ed3070
Doug Rosvick <dlr365@gmail.com>
2012-04-16 15:56:58 -0600

    t2122 - empty node variables expand to empty strings rather than an error

---
+++ test/t2122-node-var-empty.sh
@@ -0,0 +1,32 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2009-2012  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test that referencing an empty node variable isn't an error.
+
+. ./tup.sh
+
+cat > Tupfile << HERE
+: |> echo "lib list:"&(libs) > %o |> out.txt
+HERE
+
+tup touch Tupfile
+update
+
+tup_object_exist . 'echo "lib list:" > out.txt'
+
+eotup
