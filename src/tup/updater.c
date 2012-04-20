816bdd10c7b7072bd67382519a4a05f40a7fefb8
Mike Shal <marfey@gmail.com>
2012-04-19 20:19:49 -0400

    Added t8012 - ghost variant directory.
    In this case we end up with a ghost directory in the variant tree. If this later becomes a real directory in the source tree, we need to convert the variant directory from a ghost to a TUP_NODE_DIR.

--- src/tup/updater.c
+++ src/tup/updater.c
@@ -536,12 +536,32 @@ static struct tup_entry *get_rel_tent(struct tup_entry *base, struct tup_entry *
 {
 	struct tup_entry *new;
 	struct tup_entry *sub;
+	int do_mkdir = 0;
+
 	if(!tent->parent)
 		return base;
 	new = get_rel_tent(base, tent->parent);
 	if(tup_db_select_tent(new->tnode.tupid, tent->name.s, &sub) < 0)
 		return NULL;
 	if(!sub) {
+		do_mkdir = 1;
+		sub = tup_db_create_node_srcid(new->tnode.tupid, tent->name.s, TUP_NODE_DIR, tent->tnode.tupid);
+		if(!sub) {
+			fprintf(stderr, "tup error: Unable to create tup node for variant directory: ");
+			print_tup_entry(stderr, base);
+			fprintf(stderr, "\n");
+			return NULL;
+		}
+	} else {
+		if(sub->type == TUP_NODE_GHOST) {
+			if(tup_db_set_type(sub, TUP_NODE_DIR) < 0)
+				return NULL;
+			if(tup_db_set_srcid(sub, tent->tnode.tupid) < 0)
+				return NULL;
+			do_mkdir = 1;
+		}
+	}
+	if(do_mkdir) {
 		int fd;
 		fd = tup_entry_open(new);
 		if(fd < 0)
@@ -555,13 +575,6 @@ static struct tup_entry *get_rel_tent(struct tup_entry *base, struct tup_entry *
 		}
 		if(close(fd) < 0) {
 			perror("close(fd)");
-			return NULL;
-		}
-		sub = tup_db_create_node_srcid(new->tnode.tupid, tent->name.s, TUP_NODE_DIR, tent->tnode.tupid);
-		if(!sub) {
-			fprintf(stderr, "tup error: Unable to create tup node for variant directory: ");
-			print_tup_entry(stderr, base);
-			fprintf(stderr, "\n");
 			return NULL;
 		}
 	}
---
+++ test/t8012-variant-ghost-dir.sh
@@ -0,0 +1,42 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2012  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Make sure if we have a dir in the variant that is a ghost, we can still
+# create that directory in the src tree and have it work.
+. ./tup.sh
+
+tmkdir build
+
+echo "" > build/tup.config
+
+tmkdir sub
+cat > sub/Tupfile << HERE
+: |> if [ -f ghost/foo ]; then cat ghost/foo; else echo nofile; fi > %o |> output.txt
+HERE
+tup touch build/tup.config sub/Tupfile
+update
+
+echo nofile | diff - build/sub/output.txt
+
+tmkdir sub/ghost
+echo hey > sub/ghost/foo
+update
+
+echo hey | diff - build/sub/output.txt
+
+eotup
