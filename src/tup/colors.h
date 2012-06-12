0b6677e4779277f51a8f59dc9151aec1f647915c
Mike Shal <marfey@gmail.com>
2012-06-11 21:03:35 -0400

    colors: Give TUP_NODE_VAR a color, and use the correct enum.
    Using the TUP_NODE_TYPE enum allows the compiler to check that all cases are used. TUP_NODE_VAR was missing, causing print_tup_entry() to add an extra 'm' to the output.

--- src/tup/colors.h
+++ src/tup/colors.h
@@ -2,7 +2,7 @@
  *
  * tup - A file-based build system
  *
- * Copyright (C) 2010-2011  Mike Shal <marfey@gmail.com>
+ * Copyright (C) 2010-2012  Mike Shal <marfey@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -22,10 +22,11 @@
 #define tup_colors_h
 
 #include <stdio.h>
+#include "db_types.h"
 
 void color_init(void);
 void color_set(FILE *f);
-const char *color_type(int type);
+const char *color_type(enum TUP_NODE_TYPE type);
 const char *color_append_normal(void);
 const char *color_append_reverse(void);
 const char *color_reverse(void);
