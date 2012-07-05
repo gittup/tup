153027b5fdc9320b1c263edee216f49b3385e64f
Mike Shal <marfey@gmail.com>
2012-07-05 13:50:10 -0400

    Added t5076, t5077 - simple gcc -I tests.
    The t5076 test I believe is already covered, but t5077 makes sure that if we do 'gcc -Ia' and directory 'a' doesn't exist, we will re-execute when it is later created.

---
+++ test/t5077-ghost-subdir2.sh
@@ -0,0 +1,43 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2012  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Same as t5076, only the first directory doesn't exist when we do the
+# initial compilation.
+. ./tup.sh
+
+tmkdir b
+echo 'int x;' > b/foo.h
+echo '#include "foo.h"' > ok.c
+
+cat > Tupfile << HERE
+: ok.c |> gcc -c %f -o %o -Ia -Ib |> ok.o
+HERE
+tup touch b/foo.h ok.c Tupfile
+update
+
+tup_dep_exist b foo.h . 'gcc -c ok.c -o ok.o -Ia -Ib'
+sym_check ok.o x
+
+tmkdir a
+echo 'int y;' > a/foo.h
+update
+
+tup_dep_exist a foo.h . 'gcc -c ok.c -o ok.o -Ia -Ib'
+sym_check ok.o y
+
+eotup
