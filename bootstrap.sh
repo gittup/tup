837230b0b57d66b7d88da31574285d9567040fc0
Rendaw <rendaw@zarbosoft.com>
2012-09-05 21:08:40 +0900

    Reverted bootstrap.sh to original Tup version
    I accidentally included my valgrind calls to bootstrap.sh, so this
    should revert it back to how it was.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,7 @@
 CFLAGS="-g" ./build.sh
 
 if [ ! -d .tup ]; then
-	#gdb --args ./build/tup init
-	valgrind --sim-hints=fuse-compatible ./build/tup init
+	./build/tup init
 fi
-#gdb --args ./build/tup upd
-valgrind --sim-hints=fuse-compatible ./build/tup upd
+./build/tup upd
 echo "Build complete. If ./tup works, you can remove the 'build' directory."
