b2aad12bb33225138418a2aa3e26b30bf116308c
Mike Shal <marfey@gmail.com>
2012-10-08 18:28:21 -0400

    Fix color flags when using auto-detection.
    In 390eb000 I broke the part that sets the enabled flags for stdout/stderr, so stderr was always disabled. This caused the command to no longer be highlighted in read when it fails. Unfortunately this is difficult to test automatically, since redirecting the output to a file causes a different command path to execute.

--- src/tup/colors.c
+++ src/tup/colors.c
@@ -24,6 +24,12 @@
 #include <string.h>
 #include <unistd.h>
 
+enum {
+	COLOR_FILE = 0,
+	COLOR_STDOUT = 1,
+	COLOR_STDERR = 2,
+};
+
 static int enabled[3];
 static int active = 1;
 /* error_mode is set when we hightlight the whole line in red. In this case,
@@ -35,33 +41,33 @@ void color_init(void)
 {
 	const char *colors;
 	colors = tup_option_get_string("display.color");
-	enabled[0] = 0;
+	enabled[COLOR_FILE] = 0;
 	if(strcmp(colors, "never") == 0) {
-		enabled[1] = 0;
-		enabled[2] = 0;
+		enabled[COLOR_STDOUT] = 0;
+		enabled[COLOR_STDERR] = 0;
 	} else if(strcmp(colors, "always") == 0) {
-		enabled[1] = 1;
-		enabled[2] = 1;
+		enabled[COLOR_STDOUT] = 1;
+		enabled[COLOR_STDERR] = 1;
 	} else {
 		if(isatty(STDOUT_FILENO))
-			enabled[1] = 1;
+			enabled[COLOR_STDOUT] = 1;
 		else
-			enabled[2] = 0;
+			enabled[COLOR_STDOUT] = 0;
 		if(isatty(STDERR_FILENO))
-			enabled[1] = 1;
+			enabled[COLOR_STDERR] = 1;
 		else
-			enabled[2] = 0;
+			enabled[COLOR_STDERR] = 0;
 	}
 }
 
 void color_set(FILE *f)
 {
 	if(f == stdout)
-		active = 1;
+		active = COLOR_STDOUT;
 	else if(f == stderr)
-		active = 2;
+		active = COLOR_STDERR;
 	else
-		active = 0;
+		active = COLOR_FILE;
 }
 
 const char *color_type(enum TUP_NODE_TYPE type)
