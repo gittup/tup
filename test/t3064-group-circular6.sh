839a9642f97311057ed8115943b21abb9fd741ae
Mike Shal <marfey@gmail.com>
2013-02-19 15:40:38 -0500

    Added t3064 - yet another non-circular dependency test.
    This test is the reason for moving the circular dependency checking among groups until after all Tupfiles are parsed. Since there is no ordering between when new commands are added to groups and when old ones are removed, we can't know if the group graph is valid or not until after all Tupfiles have been parsed.

---
+++ test/t3064-group-circular6.sh
@@ -0,0 +1,41 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2013  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Make sure group links are cleaned up when necessary. This is tricky since
+# there is no guaranteed order between which of sub1 and sub2 will be parsed
+# first, so we have to defer circular dependency checking among groups until
+# after all files are parsed and old commands are deleted.
+. ./tup.sh
+
+tmkdir sub1
+tmkdir sub2
+
+cat > sub1/Tupfile << HERE
+: <foo> |> echo blah > %o |> output.txt | <bar>
+HERE
+update
+
+cat > sub1/Tupfile << HERE
+HERE
+cat > sub2/Tupfile << HERE
+: <bar> |> echo blah > %o |> output.txt | <foo>
+HERE
+tup touch sub1/Tupfile sub2/Tupfile
+update
+
+eotup
