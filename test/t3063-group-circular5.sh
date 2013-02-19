c61ce09493bc87d46155348f92b4639866374652
Mike Shal <marfey@gmail.com>
2013-02-19 15:38:33 -0500

    Added t3063 - another tricky non-circular dependency.
    This time we invert the group dependency by adding a new command and destroying the old one at the same time. This means we have to go through the logic to delete old commands before running the circular dependency check.

---
+++ test/t3063-group-circular5.sh
@@ -0,0 +1,33 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2013  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Same as t3062, but with a new command instead of the same command.
+. ./tup.sh
+
+cat > Tupfile << HERE
+: <foo> |> echo blah > %o |> output.txt | <bar>
+HERE
+update
+
+cat > Tupfile << HERE
+: <bar> |> echo moreinfo > %o |> newoutput.txt | <foo>
+HERE
+tup touch Tupfile
+update
+
+eotup
