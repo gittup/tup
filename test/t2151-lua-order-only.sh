bb10f9203a9c4d9f7ae5066105d6d85cc4c5adc9
Mike Shal <marfey@gmail.com>
2013-04-27 15:29:55 -0400

    Added t2151 - order-only pre-requisites for lua.

---
+++ test/t2151-lua-order-only.sh
@@ -0,0 +1,33 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2013  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Order-only for lua.
+
+. ./tup.sh
+cat > Tupfile.lua << HERE
+files = {'*.c'}
+files.order_only = tup.rule('echo blah > %o', {'foo.h'})
+tup.foreach_rule(files, 'gcc -c %f -o %o', '%B.o')
+HERE
+
+tup touch Tupfile.lua foo.c bar.c
+tup parse
+tup_sticky_exist . foo.h . "gcc -c foo.c -o foo.o"
+tup_sticky_exist . foo.h . "gcc -c bar.c -o bar.o"
+
+eotup
