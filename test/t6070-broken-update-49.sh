84e872be23877b7837e07c6b1636b2b465febcfe
Mike Shal <marfey@gmail.com>
2013-05-31 00:55:59 -0400

    Added t6070 - fix for circular group check bug.
    For the circular group check, the starting group nodes go directly into the plist (we don't have use the root node and pop it off like for the updater), so these nodes should have the 'expanded' flag set.

---
+++ test/t6070-broken-update-49.sh
@@ -0,0 +1,33 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2013  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Groups were having the wrong flags set when put into the graph, so sometimes
+# it would get tup internal error: STATE_FINISHED node 81474 in plist
+. ./tup.sh
+
+tmkdir foo
+tmkdir bar
+cat > foo/Tupfile << HERE
+: ../<A> |> touch %o |> foo.txt ../<B>
+HERE
+cat > bar/Tupfile << HERE
+: ../<B> |> touch %o |> file.txt | ../<C>
+HERE
+update
+
+eotup
