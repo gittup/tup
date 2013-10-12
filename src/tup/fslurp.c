d225be00b3bc6e39b2832a7d5e9ee34a37d7a284
Mike Shal <marfey@gmail.com>
2013-10-12 11:53:37 -0400

    Added t4157 - ignore UTF-8 byte-order-mark in Tupfiles.
    Lua already seems to handle this fine, but the Tupfile parser did not. We just need to ignore the UTF-8 BOM if it's present when we fslurp() in a file.

--- src/tup/fslurp.c
+++ src/tup/fslurp.c
@@ -2,7 +2,7 @@
  *
  * tup - A file-based build system
  *
- * Copyright (C) 2009-2011  Mike Shal <marfey@gmail.com>
+ * Copyright (C) 2009-2013  Mike Shal <marfey@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -21,6 +21,7 @@
 #include "fslurp.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <inttypes.h>
@@ -48,6 +49,9 @@ static int do_slurp(int fd, struct buf *b, int extra)
 	struct stat st;
 	char *tmp;
 	int rc;
+	int offset = 0;
+	int read_bytes;
+	int total_bytes;
 
 	if(fstat(fd, &st) < 0) {
 		perror("fstat");
@@ -60,19 +64,43 @@ static int do_slurp(int fd, struct buf *b, int extra)
 		return -1;
 	}
 
-	rc = read(fd, tmp, st.st_size);
+	read_bytes = st.st_size;
+	total_bytes = st.st_size;
+	if(read_bytes >= 3) {
+		unsigned char bom[3];
+		rc = read(fd, bom, 3);
+		if(rc != 3) {
+			perror("fslurp: read bom");
+			goto err_out;
+		}
+
+		read_bytes -= 3;
+
+		/* If the first 3 bytes are the UTF-8 byte-order mark, just
+		 * drop them from the buffer. Otherwise these are the first 3
+		 * bytes of the file, so copy them over.
+		 */
+		if(bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) {
+			total_bytes -= 3;
+		} else {
+			memcpy(tmp, bom, 3);
+			offset = 3;
+		}
+	}
+
+	rc = read(fd, tmp + offset, read_bytes);
 	if(rc < 0) {
 		perror("fslurp: read");
 		goto err_out;
 	}
-	if(rc != st.st_size) {
-		fprintf(stderr, "tup error: read %i bytes, but expected %ji bytes\n", rc, (intmax_t)st.st_size);
+	if(rc != read_bytes) {
+		fprintf(stderr, "tup error: read %i bytes, but expected %ji bytes\n", rc, (intmax_t)read_bytes);
 		errno = EIO;
 		goto err_out;
 	}
 
 	b->s = tmp;
-	b->len = st.st_size;
+	b->len = total_bytes;
 	return 0;
 
 err_out:
---
+++ test/t4157-bom.sh
@@ -0,0 +1,43 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2013  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Make sure we can handle a Tupfile with a byte-order-mark set.
+
+. ./tup.sh
+cat > bom.c << HERE
+#include <stdio.h>
+int main(void)
+{
+	printf("%c%c%c", 0xef, 0xbb, 0xbf);
+	return 0;
+}
+HERE
+gcc bom.c -o bom.exe
+(./bom.exe; echo ": foo |> cat %f > %o |> bar") > Tupfile
+echo "some text" > foo
+update
+
+echo "some text" | diff - bar
+
+rm Tupfile
+(./bom.exe; echo "tup.rule('foo', 'cat %f > %o', 'bar')") > Tupfile.lua
+update
+
+echo "some text" | diff - bar
+
+eotup
