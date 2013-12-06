6c6ed70f60483e4f5697cd3b056cd1ecd1cf39ef
Mike Shal <marfey@gmail.com>
2013-12-06 14:12:29 -0500

    Added t2173 to test extra_outputs in tup.frule()

---
+++ test/t2173-lua-extra-outputs.sh
@@ -0,0 +1,33 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2013  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Try extra_outputs in lua
+
+. ./tup.sh
+cat > ok.sh << HERE
+touch \$@ extra.txt
+HERE
+cat > Tupfile.lua << HERE
+outputs += 'foo.txt'
+outputs += 'bar.txt'
+outputs.extra_outputs += 'extra.txt'
+tup.frule{command='sh ok.sh %o', output=outputs}
+HERE
+update
+
+eotup
