5ca88e917479be5a96c7b82a3dbaf9744f2b36c3
Mike Shal <marfey@gmail.com>
2014-01-02 16:08:09 -0500

    Add build.sh support for ldpreload.

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ plat_ldflags="`pkg-config fuse --libs`"
 plat_files=""
 LDFLAGS="-lm"
 CC=gcc
+server_files="$server_files ../src/tup/server/fuse_server.c"
+server_files="$server_files ../src/tup/server/fuse_fs.c"
+server_files="$server_files ../src/tup/server/master_fork.c"
 case "$os" in
 	Linux)
 	plat_files="$plat_files ../src/compat/dummy.c"
@@ -22,6 +25,8 @@ case "$os" in
 	Darwin)
 	plat_files="$plat_files ../src/compat/clearenv.c "
 	plat_files="$plat_files ../src/compat/dir_mutex.c "
+	plat_files="$plat_files ../src/compat/open_notify.c "
+	plat_files="$plat_files ../src/compat/macosx_open.c "
 	plat_files="$plat_files ../src/compat/faccessat.c"
 	plat_files="$plat_files ../src/compat/fchmodat.c"
 	plat_files="$plat_files ../src/compat/fchownat.c"
@@ -34,10 +39,17 @@ case "$os" in
 	plat_files="$plat_files ../src/compat/symlinkat.c"
 	plat_files="$plat_files ../src/compat/unlinkat.c"
 	plat_files="$plat_files ../src/compat/utimensat.c"
+	plat_cflags=""
+	plat_ldflags=""
 	plat_cflags="$plat_cflags -include ../src/compat/macosx.h"
 	plat_cflags="$plat_cflags -DAT_SYMLINK_NOFOLLOW=0x100"
 	plat_cflags="$plat_cflags -DAT_REMOVEDIR=0x200"
+	server_files=""
+	server_files="$server_files ../src/tup/server/socketpair.c"
+	server_files="$server_files ../src/tup/server/process_depfile.c"
+	server_files="$server_files ../src/tup/server/master_fork.c"
 	CC=clang
+	LDPRELOAD=1
 	;;
 	FreeBSD)
 	plat_files="$plat_files ../src/compat/dummy.c"
@@ -51,6 +63,17 @@ echo "  mkdir build"
 mkdir -p build
 echo "  cd build"
 cd build
+
+case $LDPRELOAD in
+	1)
+	for i in ../src/ldpreload/ldpreload.c ../src/tup/access_event/send_event.c; do
+		echo "  bootstrap CC $i [ldpreload]"
+		$CC $CFLAGS -c -I../src -fPIC -include ../src/compat/macosx.h $i
+	done
+	echo "  bootstrap CC tup-ldpreload.so"
+	$CC $CFLAGS ldpreload.o send_event.o -fPIC -o tup-ldpreload.so -dynamiclib -ldl
+	;;
+esac
 
 for i in ../src/lua/*.c; do
 	echo "  bootstrap CC $CFLAGS $i"
@@ -66,7 +89,7 @@ cp ../src/luabuiltin/builtin.lua builtin.lua
 mkdir luabuiltin
 ./lua ../src/luabuiltin/xxd.lua builtin.lua luabuiltin/luabuiltin.h
 
-for i in ../src/tup/*.c ../src/tup/tup/main.c ../src/tup/monitor/null.c ../src/tup/flock/fcntl.c ../src/tup/server/fuse*.c ../src/tup/server/master_fork.c ../src/inih/ini.c $plat_files; do
+for i in ../src/tup/*.c ../src/tup/tup/main.c ../src/tup/monitor/null.c ../src/tup/flock/fcntl.c ../src/inih/ini.c $server_files $plat_files; do
 	echo "  bootstrap CC $CFLAGS $i"
 	# Put -I. first so we find our new luabuiltin.h file, not one built
 	# by a previous 'tup upd'.
