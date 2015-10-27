8035acfd18fdcc15a034afa80b5f3133d54d2dd0
Mike Shal <marfey@gmail.com>
2015-10-27 19:18:38 -0400

    Added t9007 - make sure run-scripts still use variant vardict

---
+++ test/t9007-python-variant-client.sh
@@ -0,0 +1,49 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2011-2021  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Try to use a python client.
+
+. ./tup.sh
+check_no_windows client
+check_python
+
+tmkdir build
+touch build/tup.config
+cat > foo.py << HERE
+import tup_client
+if tup_client.config_var('FOO') is not None:
+	raise Exception("No: FOO")
+if tup_client.config_var('BAR') is not None:
+	raise Exception("No: BAR")
+if tup_client.config_var('BAZ') is not None:
+	raise Exception("No: BAZ")
+HERE
+cat > Tupfile << HERE
+run PYTHONPATH=../../.. python -B \$(TUP_CWD)/foo.py
+HERE
+tup touch Tupfile
+update
+
+tup_object_exist build/tup.config BAZ
+
+varsetall FOO=y BAR=hey
+mv tup.config build
+tup touch build/tup.config
+update_fail_msg 'Exception: No: FOO'
+
+eotup
