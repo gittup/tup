e28f306f96fcd17ef15520f62d0be5a5fe92bec5
Mike Shal <marfey@gmail.com>
2015-12-03 16:36:13 -0500

    html: Simplify gen_examples.sh

--- docs/html/gen_examples.sh
+++ docs/html/gen_examples.sh
@@ -1,9 +1,8 @@
 #! /bin/sh -e
 
-echo "<h1>Examples</h1>"
 for i in $@; do
-	title=`cat $i | head -n 1`
-	synopsis=`cat $i | head -n 2 | tail -n 1`
+	title=`./gen_text.sh $i`
+	synopsis=`cat $i | head -n 1`
 	echo "<a href=\"$i\">$title</a>"
 	echo "$synopsis"
 done
