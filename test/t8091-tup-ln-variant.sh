b3ac4543a75bd0157f63f9419449d7c3fc752e0d
Mike Shal <marfey@gmail.com>
2016-06-01 15:28:21 -0400

    Added t8091 - test for !tup_ln with variants.

---
+++ test/t8091-tup-ln-variant.sh
@@ -0,0 +1,62 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2016  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Like t2188, but in a variant
+
+. ./tup.sh
+check_no_windows variant
+
+cat > Tupfile << HERE
+: foreach *.txt |> !tup_ln |> %B.lnk
+HERE
+tmkdir build
+tup touch foo.txt bar.txt build/tup.config
+
+tmkdir sub
+cat > sub/Tupfile << HERE
+: foreach *.txt |> !tup_ln |> %B.lnk
+HERE
+tup touch sub/baz.txt sub/blah.txt
+update
+
+tup_dep_exist build "$(tup_ln_cmd ../foo.txt foo.lnk)" build foo.lnk
+tup_dep_exist build "$(tup_ln_cmd ../bar.txt bar.lnk)" build bar.lnk
+
+tup_dep_exist build/sub "$(tup_ln_cmd ../../sub/baz.txt baz.lnk)" build/sub baz.lnk
+tup_dep_exist build/sub "$(tup_ln_cmd ../../sub/blah.txt blah.lnk)" build/sub blah.lnk
+
+case $tupos in
+	CYGWIN*)
+		# Windows does a copy, so it should have input dependencies
+		tup_dep_exist . foo.txt build "$(tup_ln_cmd ../foo.txt foo.lnk)"
+		tup_dep_exist . bar.txt build "$(tup_ln_cmd ../bar.txt bar.lnk)"
+
+		tup_dep_exist sub baz.txt build/sub "$(tup_ln_cmd ../../sub/baz.txt baz.lnk)"
+		tup_dep_exist sub blah.txt build/sub "$(tup_ln_cmd ../../sub/blah.txt blah.lnk)"
+		;;
+	*)
+		# Other platforms use symlink, so no input dependencies
+		tup_dep_no_exist . foo.txt build "$(tup_ln_cmd ../foo.txt foo.lnk)"
+		tup_dep_no_exist . bar.txt build "$(tup_ln_cmd ../bar.txt bar.lnk)"
+
+		tup_dep_no_exist sub baz.txt build/sub "$(tup_ln_cmd ../../sub/baz.txt baz.lnk)"
+		tup_dep_no_exist sub blah.txt build/sub "$(tup_ln_cmd ../../sub/blah.txt blah.lnk)"
+		;;
+esac
+
+eotup
