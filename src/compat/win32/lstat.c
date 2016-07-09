ca916881dd3d1d979ff197722793df7dee8452bf
nmlgc <nmlgc@nmlgc.net>
2016-07-09 03:46:13 +0200

    Windows: Reimplement lstat() using GetFileAttributesEx()
    Previously, tup used _wstat64() to retrieve the on-disk file modification
    timestamps during the updater scan, which were then checked against timestamps
    that were originally retrieved using GetFileAttributesExW(). With a
    particularly unlucky combination of shell, file system and time zone settings,
    those two functions may indeed return different timestamps for the same file,
    leading to the affected outputs being unnecessarily rebuilt.
    
    According to https://support.microsoft.com/en-us/kb/190315, the DST conversion
    of the _stat functions on Windows is by design. Since native Windows functions
    are always preferred over their C standard library wrappers and MSVCRT's
    stat() is pretty slow anyway, a custom lstat() implentation seemed to be the
    best choice here. (We could even support symlinks now if we picked an unused
    mode bit and #defined the S_IFLNK flag, but that's a different issue.)
    
    This change led to tup passing almost all of its tests when running on Cygwin
    on my system, where it previously failed at `t2019-deleted-prereq.sh`. The
    specific tests that still fail would have also failed before.

--- src/compat/win32/lstat.c
+++ src/compat/win32/lstat.c
@@ -21,13 +21,95 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <windows.h>
+#include <stdio.h>
 #include "tup/compat.h"
+
+#define WINDOWS_TICK 10000000
+#define SEC_TO_UNIX_EPOCH 11644473600LL
+
+static time_t filetime_to_epoch(FILETIME *ft)
+{
+	long long ticks = (((long long)ft->dwHighDateTime) << 32) + (long long)ft->dwLowDateTime;
+	return (time_t)(ticks / WINDOWS_TICK - SEC_TO_UNIX_EPOCH);
+}
+
+static dev_t stat_dev_for(const wchar_t *wpathname)
+{
+	DWORD len;
+	wchar_t *fullpath;
+
+	len = GetFullPathNameW(wpathname, 0, NULL, NULL);
+	fullpath = __builtin_alloca((len + 1) * sizeof(wchar_t));
+	if(!GetFullPathNameW(wpathname, len, fullpath, NULL)) {
+		fprintf(stderr, "tup error: GetFullPathNameW(\"%ls\") failed: 0x%08lx\n", wpathname, GetLastError());
+		return 0;
+	}
+	if(fullpath[1] != L':') {
+		return 0;
+	}
+	return fullpath[0] - L'A';
+}
 
 int win_lstat(const char *pathname, struct stat *buf)
 {
-	int rc;
+	WIN32_FILE_ATTRIBUTE_DATA data;
 	wchar_t wpathname[PATH_MAX];
+
 	MultiByteToWideChar(CP_UTF8, 0, pathname, -1, wpathname, PATH_MAX);
-	rc = _wstat64(wpathname, buf);
-	return rc;
+
+	if(!GetFileAttributesExW(wpathname, GetFileExInfoStandard, &data)) {
+		DWORD err = GetLastError();
+		if(err == ERROR_FILE_NOT_FOUND || err == ERROR_PATH_NOT_FOUND) {
+			errno = ENOENT;
+			return -1;
+		}
+		fprintf(stderr, "tup error: GetFileAttributesExW(\"%ls\") failed: 0x%08lx\n", wpathname, err);
+		return -1;
+	}
+
+	buf->st_dev = stat_dev_for(wpathname);
+	buf->st_rdev = buf->st_dev;
+	buf->st_nlink = 1;
+	buf->st_uid = 0;
+	buf->st_gid = 0;
+	buf->st_ino = 0;
+	buf->st_ctime = filetime_to_epoch(&data.ftCreationTime);
+	buf->st_mtime = filetime_to_epoch(&data.ftLastWriteTime);
+	buf->st_atime = filetime_to_epoch(&data.ftLastAccessTime);
+	buf->st_size = (((__int64)data.nFileSizeHigh) << 32) + (__int64)data.nFileSizeLow;
+
+	buf->st_mode = 0;
+
+	/* We could support symlinks on Windows with this, we'd just need to
+	 * #define S_IFLNK and S_ISLNK.
+	 */
+	/* if(data.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) {
+		buf->st_mode |= S_IFLNK;
+	} */
+	if(data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+		buf->st_mode |= S_IFDIR | S_IEXEC;
+	} else {
+		const char *ext = strrchr(pathname, '.');
+		if(ext) {
+			if(!stricmp(ext, ".exe") ||
+			   !stricmp(ext, ".cmd") ||
+			   !stricmp(ext, ".pif") ||
+			   !stricmp(ext, ".bat") ||
+			   !stricmp(ext, ".com")) {
+				buf->st_mode |= S_IEXEC;
+			}
+		}
+		buf->st_mode |= S_IFREG;
+	}
+
+	if(!(data.dwFileAttributes & FILE_ATTRIBUTE_READONLY)) {
+		buf->st_mode |= S_IWRITE;
+	}
+	buf->st_mode |= S_IREAD;
+
+	/* Propagate user permissions to the group/other bits. */
+	buf->st_mode |= (buf->st_mode & S_IRWXU) >> 3;
+	buf->st_mode |= (buf->st_mode & S_IRWXU) >> 6;
+
+	return 0;
 }
