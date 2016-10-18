f59407ddfffb93ad598b115d7c6c13989c093698
tharvik <tharvik@users.noreply.github.com>
2016-10-18 14:10:12 +0200

    add quote tests

---
+++ test/t2198-tupfile-spaces.sh
@@ -0,0 +1,30 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2008-2016  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Check a rule with quotes in a Tupfile
+
+. ./tup.sh
+cat > Tupfile << HERE
+: |> touch "a b" |> "a b"
+: "a b" |> touch "c d" |> "c d"
+HERE
+update
+tup_object_exist . "a b"
+tup_object_exist . "c d"
+
+eotup
