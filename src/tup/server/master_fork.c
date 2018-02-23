8140615fbb1f69e7804a030fd63529d55da7ca00
Mike Shal <marfey@gmail.com>
2018-02-23 15:41:24 -0500

    Added t5085 - verify behavior of ctrl-C.
    It turns out we should do a setpgid() for tup, so that when we pass
    along the signal it only goes to the child processes, and not any
    calling processes.

--- src/tup/server/master_fork.c
+++ src/tup/server/master_fork.c
@@ -146,6 +146,11 @@ int server_pre_init(void)
 	char c = 0;
 	int rc;
 	full_deps = tup_option_get_int("updater.full_deps");
+	if(setpgid(0, 0) < 0) {
+		perror("setpgid");
+		fprintf(stderr, "tup error: Unable to set process group for tup's subprocesses.\n");
+		return -1;
+	}
 	if(socketpair(AF_LOCAL, SOCK_STREAM, 0, msd) < 0) {
 		perror("socketpair");
 		return -1;
