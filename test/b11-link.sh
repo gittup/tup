ed8e1cd78426f2db81e703ec7b21110e158b84ed
Mike Shal <marfey@gmail.com>
2020-06-02 15:05:24 -0700

    Fix benchmark scripts.
    I'm not sure if these are at all still useful, but they should run
    successfully, at least.

--- test/b11-link.sh
+++ test/b11-link.sh
@@ -5,5 +5,5 @@ seq 1 $1 | sed 's/$/.o/' | xargs tup node
 tup node_exists . 1 1.o 50 50.o 100 100.o
 seq 1 $1 | sed 's/^/-i/' | xargs tup link "foo"
 seq 1 $1 | sed 's/^/-o/; s/$/.o/' | xargs tup link "foo"
-! tup link_exists . 1 . foo
-! tup link_exists . foo . 1.o
+! tup normal_exists . 1 . foo
+! tup normal_exists . foo . 1.o
