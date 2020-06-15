gittup/tup

commit f9000d1ac10ec014cd3e6d9f6d665bd2bbdc2666

Fix 'tup version' in bootstrapped tup.
tup_version was changed from a function to a global variable a while
ago, but bootstrap still exports it as a function. This causes 'tup
version' from the bootstrapped tup to show garbage. We can re-use
link.sh from the actual tup build to make sure we produce tup_version in
the same manner.

Fixes #384

build.sh
@@ -77,8 +77,8 @@ echo "  bootstrap CC $CFLAGS ../src/sqlite3/sqlite3.c"
 $CC $CFLAGS -c ../src/sqlite3/sqlite3.c -DSQLITE_TEMP_STORE=2 -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION $plat_cflags
 
 echo "  bootstrap LD tup $LDFLAGS"
-echo "const char *tup_version(void) {return \"$label\";}" | $CC -x c -c - -o tup_version.o
-$CC *.o -o tup -lpthread $plat_ldflags $LDFLAGS
+objs="$(echo *.o)"
+../src/tup/link.sh "$CC" "$CFLAGS -I../src" "-lpthread $plat_ldflags $LDFLAGS" "tup" "$objs"
 
 if [ "$server" = "ldpreload" ]; then
 	mkdir ldpreload

commit ed6a40575bd411d7122791019fc03a4285c30193

Pass TUP_LABEL from build.sh into link.sh for setting the version string.
This broke from f9000d1ac10ec014cd3e6d9f6d665bd2bbdc2666 - we should be
using TUP_LABEL (defaulting to "bootstrap") in the version string for
the bootstrapped binary. In particular, we shouldn't depend on git here.

Fixes #398

build.sh
@@ -78,7 +78,7 @@ $CC $CFLAGS -c ../src/sqlite3/sqlite3.c -DSQLITE_TEMP_STORE=2 -DSQLITE_THREADSAF
 
 echo "  bootstrap LD tup $LDFLAGS"
 objs="$(echo *.o)"
-../src/tup/link.sh "$CC" "$CFLAGS -I../src" "-lpthread $plat_ldflags $LDFLAGS" "tup" "$objs"
+../src/tup/link.sh "$CC" "$CFLAGS -I../src" "-lpthread $plat_ldflags $LDFLAGS" "tup" "$objs" "$label"
 
 if [ "$server" = "ldpreload" ]; then
 	mkdir ldpreload

src/tup/link.sh
@@ -4,11 +4,14 @@
 # linking, so that the version is updated whenever we change anything that
 # affects the tup binary. This used to live in the Tupfile, but to support
 # Windows local builds we need to make it an explicit shell script.
-version=`git describe`
 CC=$1
 CFLAGS=$2
 LDFLAGS=$3
 output=$4
 files=$5
+version=$6
+if [ "$version" = "" ]; then
+	version=`git describe`
+fi
 (echo "#include \"tup/version.h\""; echo "const char tup_version[] = \"$version\";") | $CC -x c -c - -o tup-version.o $CFLAGS
 $CC $files tup-version.o -o $output $LDFLAGS