2493d424a1de1f9bf47cd31fed8337fc469cf37f
Mike Shal <marfey@gmail.com>
2021-02-17 18:58:08 -0800

    Update copyright to 2021

--- src/tup/tup_lock_t.h
+++ src/tup/tup_lock_t.h
@@ -2,7 +2,7 @@
  *
  * tup - A file-based build system
  *
- * Copyright (C) 2013-2020  Mike Shal <marfey@gmail.com>
+ * Copyright (C) 2013-2021  Mike Shal <marfey@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
--- test/t2176-run-ls.sh
+++ test/t2176-run-ls.sh
@@ -1,7 +1,7 @@
 #! /bin/sh -e
 # tup - A file-based build system
 #
-# Copyright (C) 2014-2020  Mike Shal <marfey@gmail.com>
+# Copyright (C) 2014-2021  Mike Shal <marfey@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
