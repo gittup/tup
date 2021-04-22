990f76ef61146bce59095eed2a7b39ca863805c9
Mike Shal <marfey@gmail.com>
2021-04-22 10:54:31 -0700

    Added t8102 - full path !tup_ln in a variant.
    Normally all parsed files are considered in the variant, but with a full
    path input file, the input would be in the root variant.

---
+++ test/t8102-tup-ln-fullpath-variant.sh
@@ -0,0 +1,37 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2016-2021  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Same as t2196, but in a variant.
+
+. ./tup.sh
+check_no_windows variant
+
+tmkdir sub
+cat > sub/Tupfile << HERE
+: `pwd -P`/foo/input.txt |> !tup_ln |> link.txt
+HERE
+tmkdir foo
+tup touch foo/input.txt
+
+tmkdir build
+tup touch build/tup.config
+update
+
+tup_dep_exist build/sub "!tup_ln ../../foo/input.txt link.txt" build/sub link.txt
+
+eotup
