4c52d073d169836d48f4e15e7163372f6b265ee9
Mike Shal <marfey@gmail.com>
2021-04-28 15:59:14 -0700

    Added t3092 - test for a circular group condition.
    This already works, but the code path in add_sticky() isn't hit by any
    other test.

---
+++ test/t3092-group-circular8.sh
@@ -0,0 +1,36 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2021  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test for circular group links where one of the group inputs is added later.
+
+. ./tup.sh
+
+cat > Tupfile << HERE
+: |> touch foo |> foo <groupA>
+: <groupA> |> touch bar |> bar <groupB>
+HERE
+update
+
+cat > Tupfile << HERE
+: <groupB> |> touch foo |> foo <groupA>
+: <groupA> |> touch bar |> bar <groupB>
+HERE
+tup touch Tupfile
+update_fail_msg "tup error.*Circular dependency found among the following groups"
+
+eotup
