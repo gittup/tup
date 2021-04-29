6b2c469e912417b2a37ae2297a5e13a62b9f6b1b
Mike Shal <marfey@gmail.com>
2021-04-29 16:03:39 -0700

    Remove Google Analytics.
    I never looked at that stuff.

--- docs/html/gen_page.sh
+++ docs/html/gen_page.sh
@@ -1,10 +1,5 @@
 #! /bin/sh -e
 
-analytics=0
-if [ $1 = "-a" ]; then
-	analytics=1
-	shift
-fi
 menu=""
 if [ $1 = "-m" ]; then
 	shift
@@ -35,24 +30,8 @@ cat $1
 cat << HERE
 <div style="clear:both;"></div>
 </div>
-<div id="footer">&copy; 2008-2016 Mike Shal. All Rights Reserved.</div>
+<div id="footer">&copy; 2008-2021 Mike Shal. All Rights Reserved.</div>
 HERE
-
-if [ $analytics = "1" ]; then
-cat << HERE
-<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-171695-12']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script>
-HERE
-fi
 
 cat << HERE
 </body>
