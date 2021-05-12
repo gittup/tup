2f7760000341adf04d8507685572440a7a8ab852
Mike Shal <marfey@gmail.com>
2021-05-12 11:12:21 -0700

    Added t8106 - test for group expansion in variants.
    If we expand a group with %<group> in a command in a variant, we want to
    make sure that only the files in that variant are used.

---
+++ test/t8106-group-variant-expand.sh
@@ -0,0 +1,56 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2021  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Make sure expanding a group in a variant only includes files in that variant.
+
+. ./tup.sh
+check_no_windows variant
+
+tmkdir sub
+tmkdir sub2
+tmkdir sub3
+cat > sub/Tupfile << HERE
+: foreach *.h.in |> cp %f %o |> %B ../<foo-autoh>
+HERE
+cat > sub2/Tupfile << HERE
+: foreach *.h.in |> cp %f %o |> %B ../<foo-autoh>
+HERE
+cat > sub3/Tupfile << HERE
+: ../<foo-autoh> |> echo %<foo-autoh> > %o |> list.txt
+HERE
+echo '#define FOO 3' > sub/foo.h.in
+echo '#define BAR 4' > sub2/bar.h.in
+echo '#define BAZ 5' > sub2/baz.h.in
+tmkdir build
+tmkdir build2
+tup touch build/tup.config
+tup touch build2/tup.config
+update
+
+if ! grep '^[^ ]* [^ ]* [^ ]*$' build/sub3/list.txt > /dev/null; then
+	cat build/sub3/list.txt
+	echo "Error: Expected only 3 files in list.txt" 1>&2
+	exit 1
+fi
+if ! grep '^[^ ]* [^ ]* [^ ]*$' build2/sub3/list.txt > /dev/null; then
+	cat build2/sub3/list.txt
+	echo "Error: Expected only 3 files in list.txt" 1>&2
+	exit 1
+fi
+
+eotup
