324d0cd97b0d78566f19f0b30cb072eddeabd99d
Yaling Fang <fangyaling@loongson.cn>
2021-11-24 11:34:05 +0800

    Add loongarch64 info

--- src/tup/platform.c
+++ src/tup/platform.c
@@ -62,6 +62,8 @@ const char *tup_arch = "arm64";
 const char *tup_arch = "riscv64";
 #elif (__riscv || __riscv__) && __riscv_xlen == 32
 const char *tup_arch = "riscv32";
+#elif __loongarch64
+const char *tup_arch = "loongarch64";
 #else
 #error Unsupported cpu architecture. Please add support in tup/platform.c
 #endif
