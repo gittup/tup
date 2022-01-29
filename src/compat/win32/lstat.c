92ed859754626691fc259e5e6bc42c00532d96d2
Mike Shal <marfey@gmail.com>
2022-01-29 11:13:17 -0800

    Update copyright to 2022

--- src/compat/win32/lstat.c
+++ src/compat/win32/lstat.c
@@ -2,7 +2,7 @@
  *
  * tup - A file-based build system
  *
- * Copyright (C) 2015-2021  Mike Shal <marfey@gmail.com>
+ * Copyright (C) 2015-2022  Mike Shal <marfey@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
