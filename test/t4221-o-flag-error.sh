gittup/tup

commit af5ae9ab2786ffaa94a3116e28814a37dcec074b

Added t5117 - make sure mtimes are updated in the db after restoring outputs.
If we restore an output because a ^o rule failed, the rename() affects
ctime, so we need to update the db with the new time. Otherwise if the
rule gets removed before successfully building, the output file will
have a different mtime from what we think and get resurrected when it
shouldn't have been.

src/tup/updater.c
@@ -2286,6 +2286,8 @@ static int restore_outputs(struct node *n)
 	LIST_FOREACH(e, &n->edges, list) {
 		output = e->dest;
 		if(!skip_output(output->tent)) {
+			struct stat buf;
+
 			curpath[0] = '.';
 			if(snprint_tup_entry(curpath+1, sizeof(curpath)-1, output->tent) >= (int)sizeof(curpath)-1) {
 				fprintf(stderr, "tup error: curpath sized incorrectly in move_outputs()\n");
@@ -2306,6 +2308,13 @@ static int restore_outputs(struct node *n)
 					return -1;
 				}
 			}
+
+			if(lstat(curpath, &buf) != 0) {
+				fprintf(stderr, "tup error: Unable to lstat() output '%s' after restoring it.\n", curpath);
+				return -1;
+			}
+			if(tup_db_set_mtime(output->tent, MTIME(buf)) < 0)
+				return -1;
 		}
 	}
 	return 0;

test/t5117-restore-output-rule-removed.sh
@@ -1,0 +1,39 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2022  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# If we create a file using a ^o rule, then the rule to create that file fails
+# (so the file gets restored by the updater rather than removed), then remove
+# the rule, the file should be gone.
+. ./tup.sh
+cat > Tupfile << HERE
+: foreach *.c |> ^o^ gcc -c %f -o %o |> %B.o
+HERE
+
+echo "int main(void) {return 0;}" > foo.c
+update
+check_exist foo.o
+
+echo "int main(void) {return 0;} borf" > foo.c
+update_fail_msg 'expected'
+
+cat > Tupfile << HERE
+HERE
+update
+check_not_exist foo.o
+
+eotup

commit 15306e2c7ef6c31bb56956dbaf8bee52d3c2ded1

Added t4221 - test for stderr with failing ^o command.
This broke with commit af5ae9ab27 - if a command with ^o failed and
hadn't produced an expected output yet, the lstat() would fail and tup
would only show that error instead of the command's stderr. If the
lstat() fails due to ENOENT we should just skip setting the mtime.

src/tup/updater.c
@@ -2309,12 +2309,16 @@ static int restore_outputs(struct node *n)
 				}
 			}
 
-			if(lstat(curpath, &buf) != 0) {
-				fprintf(stderr, "tup error: Unable to lstat() output '%s' after restoring it.\n", curpath);
-				return -1;
+			if(lstat(curpath, &buf) == 0) {
+				if(tup_db_set_mtime(output->tent, MTIME(buf)) < 0)
+					return -1;
+			} else {
+				/* ENOENT is ok, similar to above. */
+				if(errno != ENOENT) {
+					fprintf(stderr, "tup error: Unable to lstat() output '%s' after restoring it.\n", curpath);
+					return -1;
+				}
 			}
-			if(tup_db_set_mtime(output->tent, MTIME(buf)) < 0)
-				return -1;
 		}
 	}
 	return 0;

test/t4221-o-flag-error.sh
@@ -1,0 +1,33 @@
+#! /bin/sh -e
+# tup - A file-based build system
+#
+# Copyright (C) 2022  Mike Shal <marfey@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Make sure we can actually see stderr if a command with ^o fails and hasn't
+# created the output yet.
+
+. ./tup.sh
+
+cat > ok.sh << HERE
+echo "Fail msg" 1>&2
+exit 1
+HERE
+cat > Tupfile << HERE
+: |> ^o^ sh ok.sh |> output
+HERE
+update_fail_msg "Fail msg"
+
+eotup